ab4b317c659d19121af4a97c442d38fe10ef02c7
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-04-28 22:10:54 +0200

    cleanup

---
+++ factcast-client-grpc/src/main/java/org/factcast/client/grpc/IdOnlyFact.java
@@ -0,0 +1,47 @@
+package org.factcast.client.grpc;
+
+import java.util.UUID;
+
+import org.factcast.core.Fact;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+final class IdOnlyFact implements Fact {
+    @Getter
+    @NonNull
+    final UUID id;
+
+    @Override
+    public String ns() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String type() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public UUID aggId() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String jsonHeader() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String jsonPayload() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String meta(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+}
