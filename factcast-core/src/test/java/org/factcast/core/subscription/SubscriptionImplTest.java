12410bb23cb6937eda8722c092ae0d3b3bccdfae
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-04-30 21:06:35 +0200

    coverage hunt

--- factcast-core/src/test/java/org/factcast/core/subscription/SubscriptionImplTest.java
+++ factcast-core/src/test/java/org/factcast/core/subscription/SubscriptionImplTest.java
@@ -1,5 +1,10 @@
 package org.factcast.core.subscription;
 
+import static org.junit.Assert.*;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeoutException;
+
 import org.factcast.core.Fact;
 import org.factcast.core.subscription.observer.GenericObserver;
 import org.junit.Before;
@@ -15,7 +20,7 @@ public class SubscriptionImplTest {
     private GenericObserver<Fact> observer;
 
     @InjectMocks
-    private SubscriptionImpl<Fact> subscriptionImpl;
+    private SubscriptionImpl<Fact> uut;
 
     @Before
     public void setUp() throws Exception {
@@ -24,11 +29,32 @@ public class SubscriptionImplTest {
     @Test
     public void testClose() throws Exception {
 
+        expect(TimeoutException.class, () -> uut.awaitCatchup(10));
+        expect(TimeoutException.class, () -> uut.awaitComplete(10));
+
+        uut.close();
+
+        expect(SubscriptionCancelledException.class, () -> uut.awaitCatchup(10));
+        expect(SubscriptionCancelledException.class, () -> uut.awaitComplete(10));
+
     }
 
+    private void expect(Class<? extends Throwable> ex, Callable<?> e) {
+        try {
+            e.call();
+            fail("expected " + ex);
+        } catch (Throwable actual) {
+            if (!ex.isInstance(actual)) {
+                fail("Wrong exception, expected " + ex + " but got " + actual);
+            }
+
+        }
+    }
+
+    @SuppressWarnings("resource")
     @Test(expected = NullPointerException.class)
     public void testNullConst() throws Exception {
-        new SubscriptionImpl(null);
+        new SubscriptionImpl<>(null);
     }
 
 }
