d7018a78fc39a0a358665d525456842a1ed9266e
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-05-05 22:34:38 +0200

    coverage

--- factcast-core/src/test/java/org/factcast/core/subscription/SubscriptionsTest.java
+++ factcast-core/src/test/java/org/factcast/core/subscription/SubscriptionsTest.java
@@ -1,17 +1,45 @@
 package org.factcast.core.subscription;
 
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.factcast.core.subscription.observer.GenericObserver;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SubscriptionsTest {
 
+    private GenericObserver<Integer> obs;
+
     @Before
     public void setUp() throws Exception {
+        obs = mock(GenericObserver.class);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testOn() throws Exception {
+    public void testOnNull() throws Exception {
         Subscriptions.on(null);
     }
 
+    @Test
+    public void testOn() throws Exception {
+        SubscriptionImpl<Integer> on = Subscriptions.on(obs);
+
+        verify(obs, never()).onNext(any());
+        on.notifyElement(1);
+        verify(obs).onNext(1);
+
+        verify(obs, never()).onCatchup();
+        on.notifyCatchup();
+        verify(obs).onCatchup();
+
+        verify(obs, never()).onComplete();
+        on.notifyComplete();
+        verify(obs).onComplete();
+
+        verify(obs, never()).onError(any());
+        on.notifyError(new Throwable("ignore me"));
+        verify(obs).onError(any());
+
+    }
 }
