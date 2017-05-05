d3b32f7e8df8bba6bcfc0f76028831eea90e2ec9
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-05-05 22:26:51 +0200

    tested default method map

---
+++ factcast-core/src/test/java/org/factcast/core/subscription/observer/GenericObserverTest.java
@@ -0,0 +1,46 @@
+package org.factcast.core.subscription.observer;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.factcast.core.TestFact;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenericObserverTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testMap() throws Exception {
+        GenericObserver<Integer> i = spy(new GenericObserver<Integer>() {
+
+            @Override
+            public void onNext(Integer element) {
+
+            }
+        });
+
+        FactObserver mapped = i.map(f -> 4);
+
+        verify(i, never()).onCatchup();
+        mapped.onCatchup();
+        verify(i).onCatchup();
+
+        verify(i, never()).onError(any());
+        mapped.onError(new Throwable("ignore me"));
+        verify(i).onError(any());
+
+        verify(i, never()).onComplete();
+        mapped.onComplete();
+        verify(i).onComplete();
+
+        verify(i, never()).onNext(any());
+        mapped.onNext(new TestFact());
+        verify(i).onNext(4);
+
+    }
+
+}
