45d60bc81638a5e713fe750e0d2f2a2769d57314
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-05-07 20:32:31 +0200

    added favicon

--- factcast-site/documentation/release.sh
+++ factcast-site/documentation/release.sh
@@ -9,7 +9,7 @@ hugo
 
 mkdir stage -p
 mv public/* stage
-
+cp favicon.png stage/images/
 #mv stage/index.html stage/_.html
 #cat stealth.html >stage/index.html
 rsync --delete -rcv stage/* wv:/www/docs.factcast.org
