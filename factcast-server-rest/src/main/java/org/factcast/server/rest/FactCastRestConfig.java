0912c004771a130650cbb1a16aab4b3009ae67ae
joerg.adler <joerg.adler@mercateo.com>
2017-05-10 16:34:40 +0200

    moving some code in autoconfiguration

--- factcast-server-rest/src/main/java/org/factcast/server/rest/FactCastRestConfig.java
+++ factcast-server-rest/src/main/java/org/factcast/server/rest/FactCastRestConfig.java
@@ -1,17 +1,53 @@
 package org.factcast.server.rest;
 
+import org.factcast.server.rest.resources.FactsResource;
+import org.factcast.server.rest.resources.FactsTransactionsResource;
+import org.factcast.server.rest.resources.RootResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.mercateo.common.rest.schemagen.link.LinkFactory;
+import com.mercateo.common.rest.schemagen.link.LinkMetaFactory;
+import com.mercateo.common.rest.schemagen.plugin.FieldCheckerForSchema;
+import com.mercateo.common.rest.schemagen.plugin.MethodCheckerForLink;
+import com.mercateo.rest.schemagen.spring.JerseyHateoasConfiguration;
 
 @Configuration
-@ConditionalOnMissingBean(ResourceConfig.class)
+@Import(JerseyHateoasConfiguration.class)
 public class FactCastRestConfig {
 
     @Bean
+    @ConditionalOnMissingBean(ResourceConfig.class)
     public FactCastRestApplication factCastRestApplication() {
         return new FactCastRestApplication();
     }
 
+    @Bean
+    FieldCheckerForSchema fieldCheckerForSchema() {
+        return (field, callContext) -> true;
+    }
+
+    @Bean
+    MethodCheckerForLink methodCheckerForLink() {
+        return (scope) -> true;
+    }
+
+    @Bean
+    LinkFactory<RootResource> rootResourceLinkFactory(LinkMetaFactory linkMetaFactory) {
+        return linkMetaFactory.createFactoryFor(RootResource.class);
+    }
+
+    @Bean
+    LinkFactory<FactsTransactionsResource> transactionsResourceLinkFactory(
+            LinkMetaFactory linkMetaFactory) {
+        return linkMetaFactory.createFactoryFor(FactsTransactionsResource.class);
+    }
+
+    @Bean
+    LinkFactory<FactsResource> factsResourceLinkFactory(LinkMetaFactory linkMetaFactory) {
+        return linkMetaFactory.createFactoryFor(FactsResource.class);
+    }
 }
--- factcast-server-rest/src/test/java/org/factcast/server/rest/FactCastTestServerConfiguration.java
+++ factcast-server-rest/src/test/java/org/factcast/server/rest/FactCastTestServerConfiguration.java
@@ -1,52 +1,16 @@
 package org.factcast.server.rest;
 
 import org.factcast.core.store.FactStore;
-import org.factcast.server.rest.resources.FactsResource;
-import org.factcast.server.rest.resources.FactsTransactionsResource;
-import org.factcast.server.rest.resources.RootResource;
 import org.factcast.store.inmem.InMemFactStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-import com.mercateo.common.rest.schemagen.link.LinkFactory;
-import com.mercateo.common.rest.schemagen.link.LinkMetaFactory;
-import com.mercateo.common.rest.schemagen.plugin.FieldCheckerForSchema;
-import com.mercateo.common.rest.schemagen.plugin.MethodCheckerForLink;
-import com.mercateo.rest.schemagen.spring.JerseyHateoasConfiguration;
 
 @Configuration
 @SuppressWarnings("deprecation")
-@Import(JerseyHateoasConfiguration.class)
+
 public class FactCastTestServerConfiguration {
 
     private InMemFactStore inMemFactStore = new InMemFactStore();
-
-    @Bean
-    FieldCheckerForSchema fieldCheckerForSchema() {
-        return (field, callContext) -> true;
-    }
-
-    @Bean
-    MethodCheckerForLink methodCheckerForLink() {
-        return (scope) -> true;
-    }
-
-    @Bean
-    LinkFactory<RootResource> rootResourceLinkFactory(LinkMetaFactory linkMetaFactory) {
-        return linkMetaFactory.createFactoryFor(RootResource.class);
-    }
-
-    @Bean
-    LinkFactory<FactsTransactionsResource> transactionsResourceLinkFactory(
-            LinkMetaFactory linkMetaFactory) {
-        return linkMetaFactory.createFactoryFor(FactsTransactionsResource.class);
-    }
-
-    @Bean
-    LinkFactory<FactsResource> factsResourceLinkFactory(LinkMetaFactory linkMetaFactory) {
-        return linkMetaFactory.createFactoryFor(FactsResource.class);
-    }
 
     @Bean
     FactStore getFactStore() {
