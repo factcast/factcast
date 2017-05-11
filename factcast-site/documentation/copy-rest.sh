61f14900dbfe51405fc31ca293ebf08537b2e225
joerg.adler <joerg.adler@mercateo.com>
2017-05-11 15:30:42 +0200

    improving doku

--- factcast-site/documentation/copy-rest.sh
+++ factcast-site/documentation/copy-rest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cp ../../factcast-server-rest/src/docs/api-guide.adoc content/usage/rest/api-guide.adoc 
+cp content/usage/rest/api-guide.header content/usage/rest/api-guide.adoc
+cat ../../factcast-server-rest/src/docs/api-guide.adoc >> content/usage/rest/api-guide.adoc 
 cp -r ../../factcast-server-rest/src/docs/generated-snippets .
 cp -r ../../factcast-server-rest/src/docs/snippets .
