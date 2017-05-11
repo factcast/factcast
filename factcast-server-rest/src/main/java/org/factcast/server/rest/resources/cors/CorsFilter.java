155a72256d8b43bef1ad1423da21da4a70504bf4
joerg.adler <joerg.adler@mercateo.com>
2017-05-11 16:12:55 +0200

    fixing cors

--- factcast-server-rest/src/main/java/org/factcast/server/rest/resources/cors/CorsFilter.java
+++ factcast-server-rest/src/main/java/org/factcast/server/rest/resources/cors/CorsFilter.java
@@ -14,15 +14,14 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext,
             ContainerResponseContext responseContext) throws IOException {
-        if (requestContext.getMethod().equals("OPTIONS")) {
-            responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-            responseContext.getHeaders().add("Access-Control-Allow-Headers",
-                    "origin, content-type, accept");
-            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "false");
-            responseContext.getHeaders().add("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-            responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
-        }
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers",
+                "origin, content-type, accept");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "false");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
 
     }
 }
