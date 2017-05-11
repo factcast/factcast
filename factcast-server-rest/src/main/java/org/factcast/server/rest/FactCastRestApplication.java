efbfc401ea59868ddc5c3c76833989e73c9613e5
joerg.adler <joerg.adler@mercateo.com>
2017-05-11 11:11:08 +0200

    added missing provider

--- factcast-server-rest/src/main/java/org/factcast/server/rest/FactCastRestApplication.java
+++ factcast-server-rest/src/main/java/org/factcast/server/rest/FactCastRestApplication.java
@@ -5,6 +5,7 @@ import org.factcast.server.rest.resources.FactsTransactionsResource;
 import org.factcast.server.rest.resources.RootResource;
 import org.factcast.server.rest.resources.cache.CachableFilter;
 import org.factcast.server.rest.resources.cache.NoCacheFilter;
+import org.factcast.server.rest.resources.converter.JsonParamConverterProvider;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,11 @@ public class FactCastRestApplication extends ResourceConfig {
         register(RootResource.class);
         register(FactsTransactionsResource.class);
         register(FactsResource.class);
+
         register(NoCacheFilter.class);
         register(CachableFilter.class);
+
+        register(JsonParamConverterProvider.class);
     }
 
 }
