36e9accd99da83e8ffd3b1fb32543903c04879cf
Andreas Klipp <andreas.klipp@mercateo.com>
2017-05-22 16:27:58 +0200

    db configuration over rds
    * cast DataSource, cause  rds ds factory will register the pooled ds
    under javax.sql.DataSource
    * read credentials from PooledProperties, the dbProperties are not
    filled with the credentials during the rds initialization

--- factcast-store-pgsql/src/main/java/org/factcast/store/pgsql/internal/listen/PGDriverManagerConnectionSupplier.java
+++ factcast-store-pgsql/src/main/java/org/factcast/store/pgsql/internal/listen/PGDriverManagerConnectionSupplier.java
@@ -2,7 +2,11 @@ package org.factcast.store.pgsql.internal.listen;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.function.Supplier;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.postgresql.jdbc.PgConnection;
 import org.springframework.stereotype.Component;
@@ -18,15 +22,38 @@ public class PGDriverManagerConnectionSupplier implements Supplier<PgConnection>
     @NonNull
     final org.apache.tomcat.jdbc.pool.DataSource ds;
 
+    @Inject
+    PGDriverManagerConnectionSupplier(@NonNull DataSource dataSource) {
+
+        if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
+            this.ds = (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
+        } else {
+            throw new IllegalStateException("expected "
+                    + org.apache.tomcat.jdbc.pool.DataSource.class.getName() + " , but got "
+                    + dataSource.getClass().getName());
+        }
+    }
+
+    @Override
     public PgConnection get() {
         try {
-            return (PgConnection) DriverManager.getDriver(ds.getUrl()).connect(ds.getUrl(), ds
-                    .getDbProperties());
+
+            return (PgConnection) DriverManager.getDriver(ds.getUrl()).connect(ds.getUrl(),
+                    buildCredentialProperties(ds));
         } catch (SQLException e) {
             final String msg = "Cannot acquire Connection from DriverManager: " + ds.getUrl();
             log.error(msg, e);
             throw new RuntimeException(msg, e);
         }
+    }
+
+    private Properties buildCredentialProperties(org.apache.tomcat.jdbc.pool.DataSource ds) {
+
+        Properties dbp = new Properties();
+        dbp.setProperty("user", ds.getPoolProperties().getUsername());
+        dbp.setProperty("password", ds.getPoolProperties().getPassword());
+
+        return dbp;
     }
 
 }
