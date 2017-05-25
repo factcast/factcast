3d704a854c1f6c0c569ee9ba13546d346a650049
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-05-25 15:15:51 +0200

    added PGConnectionTester0Test

---
+++ factcast-store-pgsql/src/test/java/org/factcast/store/pgsql/internal/listen/PGConnectionTester0Test.java
@@ -0,0 +1,128 @@
+package org.factcast.store.pgsql.internal.listen;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.factcast.store.pgsql.internal.metrics.PGMetricNames;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PGConnectionTester0Test {
+    @Mock
+    private MetricRegistry registry;
+
+    private PGConnectionTester uut;
+
+    @Mock
+    private PreparedStatement st;
+
+    @Mock
+    private ResultSet rs;
+
+    @Mock
+    private Counter counter;
+
+    @Before
+    public void setUp() {
+        final String fail = new PGMetricNames().connectionFailure();
+        when(registry.counter(fail)).thenReturn(counter);
+        uut = new PGConnectionTester(registry);
+    }
+
+    @Test
+    public void testTestPositive() throws Exception {
+
+        Connection c = mock(Connection.class);
+        when(c.prepareStatement(anyString())).thenReturn(st);
+        when(st.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true, false);
+        when(rs.getInt(1)).thenReturn(42);
+
+        boolean test = uut.test(c);
+        assertTrue(test);
+        verifyZeroInteractions(counter);
+    }
+
+    @Test
+    public void testTestFailure() throws Exception {
+
+        Connection c = mock(Connection.class);
+        when(c.prepareStatement(anyString())).thenReturn(st);
+        when(st.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true, false);
+        when(rs.getInt(1)).thenReturn(1);
+
+        boolean test = uut.test(c);
+        assertFalse(test);
+        verify(counter).inc();
+    }
+
+    @Test
+    public void testTestException1() throws Exception {
+
+        Connection c = mock(Connection.class);
+        when(c.prepareStatement(anyString())).thenReturn(st);
+        when(st.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true, false);
+        when(rs.getInt(1)).thenThrow(new SQLException("BAM"));
+
+        boolean test = uut.test(c);
+        assertFalse(test);
+        verify(counter).inc();
+    }
+
+    @Test
+    public void testTestException2() throws Exception {
+
+        Connection c = mock(Connection.class);
+        when(c.prepareStatement(anyString())).thenReturn(st);
+        when(st.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenThrow(new SQLException("BAM"));
+
+        boolean test = uut.test(c);
+        assertFalse(test);
+        verify(counter).inc();
+    }
+
+    @Test
+    public void testTestException3() throws Exception {
+
+        Connection c = mock(Connection.class);
+        when(c.prepareStatement(anyString())).thenReturn(st);
+        when(st.executeQuery()).thenThrow(new SQLException("BAM"));
+
+        boolean test = uut.test(c);
+        assertFalse(test);
+        verify(counter).inc();
+    }
+
+    @Test
+    public void testTestException4() throws Exception {
+
+        Connection c = mock(Connection.class);
+        when(c.prepareStatement(anyString())).thenThrow(new SQLException("BAM"));
+
+        boolean test = uut.test(c);
+        assertFalse(test);
+        verify(counter).inc();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testPGConnectionTester() throws Exception {
+        new PGConnectionTester(null);
+    }
+
+}
