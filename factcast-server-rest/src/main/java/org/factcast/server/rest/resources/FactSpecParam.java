cd21092f70593a715a28265fb358ae2ac226a634
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-05-26 11:59:07 +0200

    coverage

--- factcast-server-rest/src/main/java/org/factcast/server/rest/resources/FactSpecParam.java
+++ factcast-server-rest/src/main/java/org/factcast/server/rest/resources/FactSpecParam.java
@@ -6,22 +6,30 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 
 /**
  * Defines a Specification of facts to match for a subscription.
  */
-@Data
+@Getter
 public class FactSpecParam {
+    @Setter
+    @NonNull
     @JsonProperty
     private String ns;
 
+    @Setter
     @JsonProperty
     private String type = null;
 
+    @Setter
     @JsonProperty
     private UUID aggId = null;
 
     @JsonProperty
     private final Map<String, String> meta = new HashMap<>();
+
+    // TODO JAR: where is the jsFilterScript !?
 }
