aa0e4c52a7fd0562812d8eb34e948a665ecc286c
Uwe Schaefer <uwe.schaefer@mercateo.com>
2017-05-30 17:23:15 +0200

    uups... debugging change leaked through

--- factcast-store-test/src/main/java/org/factcast/store/test/AbstractFactStore0Test.java
+++ factcast-store-test/src/main/java/org/factcast/store/test/AbstractFactStore0Test.java
@@ -195,7 +195,7 @@ public abstract class AbstractFactStore0Test {
 
     }
 
-    @Test(timeout = 1000000)
+    @Test(timeout = 10000)
     @DirtiesContext
     public void testEmptyStoreFollowWithCancel() throws Exception {
         TestFactObserver observer = testObserver();
@@ -440,7 +440,8 @@ public abstract class AbstractFactStore0Test {
                 + "\",\"type\":\"someType\",\"ns\":\"default\"}", "{}"));
 
         FactObserver observer = mock(FactObserver.class);
-        uut.subscribeToFacts(SubscriptionRequest.catchup(FactSpec.ns("default")).skipMarks()
+        uut.subscribeToFacts(SubscriptionRequest.catchup(FactSpec.ns("default"))
+                .skipMarks()
                 .fromScratch(), observer).awaitComplete();
 
         verify(observer, times(1)).onNext(any());
@@ -458,7 +459,8 @@ public abstract class AbstractFactStore0Test {
 
         FactObserver observer = mock(FactObserver.class);
         uut.subscribeToFacts(SubscriptionRequest.catchup(FactSpec.ns("default").aggId(aggId1))
-                .skipMarks().fromScratch(), observer).awaitComplete();
+                .skipMarks()
+                .fromScratch(), observer).awaitComplete();
 
         verify(observer, times(1)).onNext(any());
 
@@ -477,13 +479,15 @@ public abstract class AbstractFactStore0Test {
 
         FactObserver observer = mock(FactObserver.class);
         uut.subscribeToFacts(SubscriptionRequest.catchup(FactSpec.ns("default").aggId(aggId1))
-                .skipMarks().fromScratch(), observer).awaitComplete();
+                .skipMarks()
+                .fromScratch(), observer).awaitComplete();
 
         verify(observer, times(1)).onNext(any());
 
         observer = mock(FactObserver.class);
         uut.subscribeToFacts(SubscriptionRequest.catchup(FactSpec.ns("default").aggId(aggId2))
-                .skipMarks().fromScratch(), observer).awaitComplete();
+                .skipMarks()
+                .fromScratch(), observer).awaitComplete();
 
         verify(observer, times(1)).onNext(any());
 
@@ -502,7 +506,8 @@ public abstract class AbstractFactStore0Test {
 
         FactObserver observer = mock(FactObserver.class);
         uut.subscribeToFacts(SubscriptionRequest.catchup(FactSpec.ns("default").aggId(aggId2))
-                .skipMarks().fromScratch(), observer).awaitComplete();
+                .skipMarks()
+                .fromScratch(), observer).awaitComplete();
 
         verify(observer, times(1)).onNext(any());
 
