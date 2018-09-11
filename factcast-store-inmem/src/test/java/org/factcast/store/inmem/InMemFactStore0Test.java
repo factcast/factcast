4fd38dc8dc9180a93f4443acc996161638b1af74
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2018-09-11 09:33:46 +0200

    Issue87 (#105)
    * #87: marked for re-review
    * #87: removed .clear on InMemFactStore, closes #87

--- factcast-store-inmem/src/test/java/org/factcast/store/inmem/InMemFactStore0Test.java
+++ factcast-store-inmem/src/test/java/org/factcast/store/inmem/InMemFactStore0Test.java
@@ -1,14 +1,10 @@
 package org.factcast.store.inmem;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-import java.util.Arrays;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
-import org.factcast.core.DefaultFact;
-import org.factcast.core.Fact;
 import org.factcast.core.store.FactStore;
 import org.factcast.store.test.AbstractFactStore0Test;
 import org.junit.Test;
@@ -32,17 +28,4 @@ public class InMemFactStore0Test extends AbstractFactStore0Test {
         inMemFactStore.destroy();
         verify(es).shutdown();
     }
-
-    @Test
-    public void testClear() throws Exception {
-        Fact f1 = DefaultFact.of("{\"id\":\"" + UUID.randomUUID().toString() + "\"}", "{}");
-        assertEquals(0, store.store.size());
-
-        uut.publish(Arrays.asList(f1));
-        assertEquals(1, store.store.size());
-
-        store.clear();
-        assertEquals(0, store.store.size());
-    }
-
 }
