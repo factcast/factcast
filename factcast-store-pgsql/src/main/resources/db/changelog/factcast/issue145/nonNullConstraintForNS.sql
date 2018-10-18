97141a12f85f8c3f12ce629895dffa7bd6067814
Uwe Schaefer <uwe.schaefer@mercateo.com>
2018-10-18 14:34:57 +0200

    fixed typo

--- factcast-store-pgsql/src/main/resources/db/changelog/factcast/issue145/nonNullConstraintForNS.sql
+++ factcast-store-pgsql/src/main/resources/db/changelog/factcast/issue145/nonNullConstraintForNS.sql
@@ -4,4 +4,4 @@ update fact
    set header= jsonb_set(header ,'{ns}' ,'"default"'::jsonb, true)
    where header->'ns' is null;
 alter table fact add constraint mandatory_ns CHECK (header ? 'ns');
-alter table fact add constraint mandatory_ns CHECK (header ? 'id');
+alter table fact add constraint mandatory_id CHECK (header ? 'id');
