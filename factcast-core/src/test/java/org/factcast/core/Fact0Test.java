2c1ca886f7313427b6e541e1225c84dee6bb2401
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2018-10-19 15:43:45 +0200

    #147: provide build for fact (#150)

--- factcast-core/src/test/java/org/factcast/core/Fact0Test.java
+++ factcast-core/src/test/java/org/factcast/core/Fact0Test.java
@@ -1,6 +1,11 @@
 package org.factcast.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.UUID;
 
 import org.junit.Test;
 
@@ -29,4 +34,40 @@ public class Fact0Test {
         assertEquals(f.id(), f2.id());
     }
 
+    @Test
+    public void testBuilderDefaults() throws Exception {
+        Fact f = Fact.builder().build("{\"a\":1}");
+        assertEquals("default", f.ns());
+        assertNotNull(f.id());
+        assertEquals("{\"a\":1}", f.jsonPayload());
+    }
+
+    @Test
+    public void testBuilder() throws Exception {
+        UUID aggId1 = UUID.randomUUID();
+        UUID aggId2 = UUID.randomUUID();
+        UUID aggId3 = UUID.randomUUID();
+        UUID factId = UUID.randomUUID();
+
+        Fact f = Fact.builder()
+                .ns("ns")
+                .type("type")
+                .aggId(aggId1)
+                .aggId(aggId2)
+                .aggId(aggId3)
+                .meta("foo", "bar")
+                .meta("buh", "bang")
+                .id(factId)
+                .build("{\"a\":2}");
+
+        assertEquals("ns", f.ns());
+        assertEquals("type", f.type());
+        assertTrue(f.aggIds().contains(aggId1));
+        assertTrue(f.aggIds().contains(aggId2));
+        assertTrue(f.aggIds().contains(aggId3));
+        assertFalse(f.aggIds().contains(factId));
+        assertEquals("bar", f.meta("foo"));
+        assertEquals("bang", f.meta("buh"));
+        assertEquals("{\"a\":2}", f.jsonPayload());
+    }
 }
