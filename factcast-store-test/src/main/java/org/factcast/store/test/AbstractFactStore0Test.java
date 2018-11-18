ef54301fb07d0ace02ba1c1b16bd2f8aa018a878
Uwe Schaefer <uwe@codesmell.de>
2018-11-18 20:54:50 +0100

    minor test cleanup

--- factcast-store-test/src/main/java/org/factcast/store/test/AbstractFactStore0Test.java
+++ factcast-store-test/src/main/java/org/factcast/store/test/AbstractFactStore0Test.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Mercateo AG (http://www.mercateo.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -705,6 +705,8 @@ public abstract class AbstractFactStore0Test {
         // needs to fail
         assertFalse(l.get().await(500, TimeUnit.MILLISECONDS));
         uut.publish(Fact.builder().ns("followtest").id(UUID.randomUUID()).build("{}"));
-        l.get().await();
+
+        assertTrue(l.get().await(10, TimeUnit.SECONDS),
+                "failed to see all the facts published within 10 seconds.");
     }
 }
