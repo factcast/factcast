b4d5548c0636c461ff746fba9e5f3d127e6d58aa
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2018-11-20 01:28:39 +0100

    fixed empty catch block (#232)

--- factcast-server-grpc/src/main/java/org/factcast/server/grpc/FactStoreGrpcService.java
+++ factcast-server-grpc/src/main/java/org/factcast/server/grpc/FactStoreGrpcService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Mercateo AG (http://www.mercateo.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
  */
 package org.factcast.server.grpc;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
@@ -157,7 +156,9 @@ public class FactStoreGrpcService extends RemoteFactStoreImplBase {
                     if (v != null)
                         implVersion = v;
                 }
-            } catch (IOException e) {
+            } catch (Exception ignore) {
+                // whatever fails when reading the version implies, that the impl Version is
+                // "UNKNOWN"
             }
         properties.put(Capabilities.FACTCAST_IMPL_VERSION.toString(), implVersion);
     }
