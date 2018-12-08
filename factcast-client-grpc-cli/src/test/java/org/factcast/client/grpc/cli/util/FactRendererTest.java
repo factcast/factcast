df0aedd61d466e7e23db6b70e1c1c524f5177b96
Uwe Schaefer <uwe.schaefer@mercateo.com>
2018-12-08 23:29:06 +0100

    minor coverage improvements

---
+++ factcast-client-grpc-cli/src/test/java/org/factcast/client/grpc/cli/util/FactRendererTest.java
@@ -0,0 +1,59 @@
+package org.factcast.client.grpc.cli.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.factcast.client.grpc.cli.util.Parser.Options;
+import org.factcast.core.Fact;
+import org.junit.jupiter.api.Test;
+
+import lombok.NonNull;
+
+public class FactRendererTest {
+
+    Fact f = Fact.builder()
+            .id(new UUID(0L, 0L))
+            .ns("ns")
+            .aggId(new UUID(0L, 1L))
+            .meta("foo", "bar")
+            .type("type")
+            .build("{\"some\":\"json\"}");
+
+    @Test
+    void testRender() throws Exception {
+        FactRenderer uut = new FactRenderer(new Options());
+        assertEquals("Fact: id=00000000-0000-0000-0000-000000000000\n" +
+                "	header: {\"id\":\"00000000-0000-0000-0000-000000000000\",\"ns\":\"ns\",\"type\":\"type\",\"aggIds\":[\"00000000-0000-0000-0000-000000000001\"],\"meta\":{\"foo\":\"bar\"}}\n"
+                +
+                "	payload: {\"some\":\"json\"}\n" +
+                "\n", uut.render(f));
+    }
+
+    @Test
+    void testRenderPretty() throws Exception {
+        Options options = new Options() {
+            @Override
+            public boolean pretty() {
+                return true;
+            }
+        };
+
+        FactRenderer uut = new FactRenderer(options);
+        assertEquals("Fact: id=00000000-0000-0000-0000-000000000000\n" +
+                "	header: {\n" +
+                "		  \"id\" : \"00000000-0000-0000-0000-000000000000\",\n" +
+                "		  \"ns\" : \"ns\",\n" +
+                "		  \"type\" : \"type\",\n" +
+                "		  \"aggIds\" : [ \"00000000-0000-0000-0000-000000000001\" ],\n" +
+                "		  \"meta\" : {\n" +
+                "		    \"foo\" : \"bar\"\n" +
+                "		  }\n" +
+                "		}\n" +
+                "	payload: {\n" +
+                "		  \"some\" : \"json\"\n" +
+                "		}\n" +
+                "\n" +
+                "", uut.render(f));
+    }
+}
