d170d1b783b573928fc1b213f48c18713c6f6d98
Uwe Schaefer <uwe.schaefer@mercateo.com>
2019-04-14 12:02:55 +0200

    #55-basicauth: improve coverage

--- factcast-core/src/main/java/org/factcast/core/util/FactCastJson.java
+++ factcast-core/src/main/java/org/factcast/core/util/FactCastJson.java
@@ -72,7 +72,7 @@ public final class FactCastJson {
     }
 
     @SneakyThrows
-    public static <T> T readValue(@NonNull Class<T> class1, InputStream json) {
+    public static <T> T readValue(@NonNull Class<T> class1, @NonNull InputStream json) {
         return reader.forType(class1).readValue(json);
     }
 
