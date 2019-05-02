822038e4d069ae5bb923a3c941f6618ed4f5f8e0
Andreas Klipp <andreas.klipp@mercateo.com>
2019-05-02 20:56:23 +0200

    postgres module uuid-oosp documentation
    * add brackets

--- factcast-store-pgsql/src/main/resources/db/changelog/factcast/issue372/createApplicationUser.sql
+++ factcast-store-pgsql/src/main/resources/db/changelog/factcast/issue372/createApplicationUser.sql
@@ -3,5 +3,5 @@
 
 CREATE USER "factcast-application-user" WITH PASSWORD 'factcast-application-user';
 GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE fact, tokenstore, catchup TO "factcast-application-user";
-GRANT EXECUTE ON  FUNCTION notifyFactInsert TO "factcast-application-user";
+GRANT EXECUTE ON  FUNCTION notifyFactInsert() TO "factcast-application-user";
 GRANT USAGE,SELECT ON SEQUENCE catchup_seq, fact_ser_seq TO "factcast-application-user";
