46bc596827b6f83129cddd95485aa7294073da23
Uwe Schaefer <uwe.schaefer@mercateo.com>
2019-07-14 08:20:55 +0200

    relocate jackson

--- factcast-store-inmem/src/main/java/org/factcast/store/inmem/InMemFact.java
+++ factcast-store-inmem/src/main/java/org/factcast/store/inmem/InMemFact.java
@@ -19,28 +19,13 @@ import org.factcast.core.DefaultFact;
 import org.factcast.core.Fact;
 import org.factcast.core.util.FactCastJson;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 class InMemFact extends DefaultFact {
 
     public InMemFact(long ser, Fact toCopyFrom) {
-        super(addSerToHeader(ser, toCopyFrom.jsonHeader()), toCopyFrom.jsonPayload());
-    }
-
-    private static String addSerToHeader(long ser, String jsonHeader) {
-        ObjectNode json = FactCastJson.toObjectNode(jsonHeader);
-        ObjectNode meta = (ObjectNode) json.get("meta");
-        if (meta == null) {
-            // create a new node
-            meta = FactCastJson.newObjectNode();
-            json.set("meta", meta);
-        }
-        // set ser as attribute _ser
-        meta.put("_ser", ser);
-        return json.toString();
+        super(FactCastJson.addSerToHeader(ser, toCopyFrom.jsonHeader()), toCopyFrom.jsonPayload());
     }
 
     @Override
