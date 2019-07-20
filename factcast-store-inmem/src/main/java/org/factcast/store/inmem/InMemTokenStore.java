95490c7bffc04d74d0c1873f4b7d9c7b9736776a
Uwe Schaefer <uwe.schaefer@mercateo.com>
2019-07-20 14:57:47 +0200

    churn

--- factcast-store-inmem/src/main/java/org/factcast/store/inmem/InMemTokenStore.java
+++ factcast-store-inmem/src/main/java/org/factcast/store/inmem/InMemTokenStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 factcast (http://factcast.org)
+ * Copyright © 2019 factcast
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
