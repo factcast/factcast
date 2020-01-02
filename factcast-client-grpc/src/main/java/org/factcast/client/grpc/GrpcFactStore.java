c74965f1bdd31cb0a8a3891ea10d249acf38a4ef
Uwe Schaefer <uwe.schaefer@mercateo.com>
2020-01-02 10:31:08 +0100

    updated license headers

--- factcast-client-grpc/src/main/java/org/factcast/client/grpc/GrpcFactStore.java
+++ factcast-client-grpc/src/main/java/org/factcast/client/grpc/GrpcFactStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019 factcast
+ * Copyright © 2017-2020 factcast.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
