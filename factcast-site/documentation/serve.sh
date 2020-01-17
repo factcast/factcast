a9c7aa4a4fb090900d2269eeb68135e0100d3294
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2020-01-17 18:02:00 +0100

    run docker from maven via exec plugin

--- factcast-site/documentation/serve.sh
+++ factcast-site/documentation/serve.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-int_trap() {
- echo ""
-}
-
-
-trap int_trap INT
-
-rm -rf stage
-rm -rf public
-rm -rf themes
-
-#(
-# wget -qO- https://github.com/uweschaefer/hugo-theme-docdock/archive/master.zip|bsdtar -xvf- 
-#)
-
 (
 docker run --rm -u `id -u` -v $PWD:/srv/hugo yanqd0/hugo hugo
 cd public
