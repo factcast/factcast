cc91ec641863e094f440dbc4384e0e75582f69c0
Uwe Schaefer <uwe.schaefer@mercateo.com>
2020-01-18 23:53:32 +0100

    #614: use huge server

--- factcast-site/documentation/serve.sh
+++ factcast-site/documentation/serve.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 (
-docker run --rm -u `id -u` -v $PWD:/srv/hugo yanqd0/hugo hugo
-cd public
-python -m SimpleHTTPServer 8080
+docker run --rm -u `id -u` -p 1313:1313 -v $PWD:/srv/hugo yanqd0/hugo hugo server -w --bind 0.0.0.0
 )
 
