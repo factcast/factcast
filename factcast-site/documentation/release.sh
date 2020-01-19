8c8280bef4c4aaa8666a0d322b395d61cde26cb8
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2020-01-19 20:09:56 +0100

    #614 remove hugo binaries (#620)
    * run docker from maven via exec plugin
    
    * #614: formatting
    
    * #614: use huge server
    
    * #614: disable hugo on circle
    
    * #614: disable hugo on circle
    
    * #614: removed profile switch
    
    * #614: cosmetics

--- factcast-site/documentation/release.sh
+++ factcast-site/documentation/release.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-set -e
-rm -rf stage
-rm -rf public
-
-docker run --rm -u `id -u` -v $PWD:/srv/hugo yanqd0/hugo hugo
-
-mkdir stage -p
-cp -r public/* stage
-cp favicon.png stage/images/
-rsync --delete -rcv stage/* con:/www/docs.factcast.org
-rm -rf stage
-
-
-
+cp favicon.png public/images/
+rsync --delete -rcv public/* con:/www/docs.factcast.org
--- factcast-site/documentation/serve.sh
+++ factcast-site/documentation/serve.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-int_trap() {
- echo ""
-}
-
-
-trap int_trap INT
-
-rm -rf stage
-rm -rf public
-rm -rf themes
-
-#(
-# wget -qO- https://github.com/uweschaefer/hugo-theme-docdock/archive/master.zip|bsdtar -xvf- 
-#)
-
 (
-docker run --rm -u `id -u` -v $PWD:/srv/hugo yanqd0/hugo hugo
-cd public
-python -m SimpleHTTPServer 8080
+docker run --rm -u `id -u` -p 1313:1313 -v $PWD:/srv/hugo yanqd0/hugo hugo server -w --bind 0.0.0.0
 )
 
