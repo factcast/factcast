e62d9c10dfcb7a144ac054f044b7069a67f90600
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2020-03-24 18:22:13 +0100

    #715-transformation-cache: removed unused dependency in non-component bean

--- factcast-store-pgsql/src/main/resources/db/changelog/factcast/issue715/transformationCache.sql
+++ factcast-store-pgsql/src/main/resources/db/changelog/factcast/issue715/transformationCache.sql
@@ -1,6 +1,14 @@
 CREATE TABLE transformationcache(
 	cache_key 		varchar(2048) PRIMARY KEY,
+	
+---- text is used here over jsonb in order to take load off the database that might occur when 
+---- unnecessarily parsing the data.	
+	
 	header 			text NOT NULL,
 	payload 		text NOT NULL,
+
+----   last_access is intentionally not indexed, due to the fact, that it will be very frequently 
+----   updated and cleanup can safely use a FTS, due to its nature (it is async)
+	
 	last_access 		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
