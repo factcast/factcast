a3cfcf0bcf53617e1f34c4f034047ecfc244209a
Uwe Schaefer <uwe.schaefer@mercateo.com>
2020-03-31 23:21:36 +0200

    #660: initial setup

---
+++ factcast-schema-registry-maven-plugin/src/main/java/org/factcast/schema/registry/plugin/BuildMojo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2017-2020 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.schema.registry.plugin;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "build", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class BuildMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+    private File outputDirectory;
+
+    public void execute()
+            throws MojoExecutionException {
+        File f = outputDirectory;
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+        org.factcast.schema.registry.cli.Application.main(new String[] {});
+    }
+
+}
