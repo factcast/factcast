5eef886d75899f570bece143a34f2dc5435e6347
Bernhard Streit (Extern) <Bernhard.Streit.Ext@prisma-capacity.eu>
2020-08-28 12:08:21 +0200

    Test for SnapshotCache

--- factcast-store-pgsql/src/test/resources/test_schema.sql
+++ factcast-store-pgsql/src/test/resources/test_schema.sql
@@ -95,12 +95,28 @@ CREATE INDEX IF NOT EXISTS idx_transformationstore on transformationstore(ns,typ
 CREATE TABLE IF NOT EXISTS shedlock(name VARCHAR(64) NOT NULL, lock_until TIMESTAMP NOT NULL,
     locked_at TIMESTAMP NOT NULL, locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name));
 
-CREATE TABLE IF NOT EXISTS snapshot_cache(
+create table IF NOT EXISTS snapshot_cache (
 	cache_key 		varchar(2048) not null,
-	uuid     		uuid,
+
+-- note that the UUID is part of the primary key, so it is not nullable.
+-- while it makes sense to use a separate uuid when storing aggregate states, it is
+-- completely useless with projection states. In this case we store a fake uuid(0,0) in order to maintain the PK constraint
+
+	uuid     		uuid not null,
+
+-- represents the state of the blob (processed all relevant facts up to factid)
+
 	factid          uuid not null,
+
 	data 			bytea not null,
+
+-- indicated if the data is already compressed in order to bypass transport compression if possible
+
+	compressed      boolean not null,
+
 	last_access     timestamp with time zone default now() not null,
 	created_at    	timestamp with time zone default now() not null,
+
     primary key (uuid,cache_key)
 );
+
