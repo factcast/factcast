d051ea71d61b47619300166655fae934d0985fb2
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-11-14 10:56:22 +0100

    #1092_whitelist: abstract base mojo

---
+++ factcast-schema-registry-maven-plugin/src/main/java/org/factcast/schema/registry/plugin/mojo/AbstractBaseMojo.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2017-2020 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.schema.registry.plugin.mojo;
+
+import java.io.File;
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.factcast.schema.registry.plugin.CliArgumentBuilder;
+import org.factcast.schema.registry.plugin.WhiteListFileCreator;
+
+public abstract class AbstractBaseMojo extends AbstractMojo {
+  @Parameter(defaultValue = "${project.build.directory}/registry", required = true)
+  protected File outputDirectory;
+
+  @Parameter(defaultValue = "${project.basedir}/src/main/resources", required = true)
+  protected File sourceDirectory;
+
+  @Parameter(property = "includedEvents")
+  protected List<String> includedEvents;
+
+  @Inject protected WhiteListFileCreator whiteListFileCreator;
+
+  @Inject protected CliArgumentBuilder argumentBuilder;
+
+  protected void checkSourceDirectory() {
+    if (!sourceDirectory.exists())
+      throw new IllegalArgumentException(
+          "Source directory (property 'sourceDirectory') does not exist: "
+              + sourceDirectory.getPath());
+  }
+}
