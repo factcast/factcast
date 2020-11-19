e68561f25f633f3cedd71c44c1d6063e5c644160
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-11-19 19:05:22 +0100

    #1092_whitelist: added globbing support

--- factcast-schema-registry-cli/src/test/kotlin/org/factcast/schema/registry/cli/whitelistfilter/WhiteListFilterTest.kt
+++ factcast-schema-registry-cli/src/test/kotlin/org/factcast/schema/registry/cli/whitelistfilter/WhiteListFilterTest.kt
@@ -6,6 +6,7 @@ import org.factcast.schema.registry.cli.project.structure.EventFolder
 import org.factcast.schema.registry.cli.project.structure.EventVersionFolder
 import org.factcast.schema.registry.cli.project.structure.NamespaceFolder
 import org.factcast.schema.registry.cli.project.structure.ProjectFolder
+import java.nio.file.FileSystems
 import java.nio.file.Paths
 
 class WhiteListFilterTest : StringSpec() {
@@ -20,7 +21,7 @@ class WhiteListFilterTest : StringSpec() {
                     ))
             ))
 
-            val whiteList = listOf("shipping/OrderShipped/versions/1")
+            val whiteList = listOf("/shipping/OrderShipped/versions/1")
             val filteredProject = filterProject(project, whiteList)
 
             filteredProject.namespaces[0].eventFolders[0].versionFolders.size shouldBe 1
@@ -31,36 +32,123 @@ class WhiteListFilterTest : StringSpec() {
                     createNamespaceFolder("shipping", listOf(
                             createEventFolder("shipping", "OrderShipped", listOf(
                                     createEventVersionFolder("shipping", "OrderShipped", 1),
-                                    createEventVersionFolder("shipping", "OrderShipped", 2), // not whitelisted
-
+                                    createEventVersionFolder("shipping", "OrderShipped", 2) // not whitelisted
                             ))
                     ))
             ))
 
-            val whiteList = listOf("shipping/OrderShipped/versions/1")
+            val whiteList = listOf("/shipping/OrderShipped/versions/1")
             val filteredProject = filterProject(project, whiteList)
 
             filteredProject.namespaces[0].eventFolders[0].versionFolders.size shouldBe 1
             filteredProject.namespaces[0].eventFolders[0].versionFolders[0].path.endsWith("shipping/OrderShipped/versions/1") shouldBe true
         }
 
+        "whiteListInTwoDifferentContexts" {
+            val project = createProjectFolder(listOf(
+                    createNamespaceFolder("ordering", listOf(
+                            createEventFolder("ordering", "OrderReceived", listOf(
+                                    createEventVersionFolder("ordering", "OrderReceived", 1),
+                                    createEventVersionFolder("ordering", "OrderReceived", 2)
+                            )),
+                            createEventFolder("ordering", "OrderProcessed", listOf(
+                                    createEventVersionFolder("ordering", "OrderProcessed", 1),
+                                    createEventVersionFolder("ordering", "OrderProcessed", 2)
+                            ))
+                    )),
+                    createNamespaceFolder("shipping", listOf(
+                            createEventFolder("shipping", "OrderShipped", listOf(
+                                    createEventVersionFolder("shipping", "OrderShipped", 1),
+                                    createEventVersionFolder("shipping", "OrderShipped", 2)
+                            ))
+                    ))
+            ))
 
+            val whiteList = listOf(
+                    "/ordering/OrderReceived/versions/1",
+                    "/ordering/OrderProcessed/versions/1",
+                    "/shipping/OrderShipped/versions/1"
+            )
+            val filteredProject = filterProject(project, whiteList)
+
+            filteredProject.namespaces[0].eventFolders[0].versionFolders.size shouldBe 1
+            filteredProject.namespaces[0].eventFolders[0].versionFolders[0].path.endsWith("ordering/OrderReceived/versions/1") shouldBe true
+
+            filteredProject.namespaces[0].eventFolders[1].versionFolders.size shouldBe 1
+            filteredProject.namespaces[0].eventFolders[1].versionFolders[0].path.endsWith("ordering/OrderProcessed/versions/1") shouldBe true
+
+            filteredProject.namespaces[1].eventFolders[0].versionFolders.size shouldBe 1
+            filteredProject.namespaces[1].eventFolders[0].versionFolders[0].path.endsWith("shipping/OrderShipped/versions/1") shouldBe true
+        }
+
+        "specialCaseIfNoWhiteListEntryMatchesTheEventIsRemovedCompletely" {
+            val project = createProjectFolder(listOf(
+                    createNamespaceFolder("shipping", listOf(
+                            createEventFolder("shipping", "OrderShipped", listOf(
+                                    createEventVersionFolder("shipping", "OrderShipped", 1)
+                            ))
+                    ))
+            ))
+
+            val filteredProject = filterProject(project, emptyList())
+            filteredProject.namespaces[0].eventFolders.size shouldBe 0
+        }
+
+        "whiteListSupportsWildCards" {
+            val project = createProjectFolder(listOf(
+                    createNamespaceFolder("shipping", listOf(
+                            createEventFolder("shipping", "OrderShipped", listOf(
+                                    createEventVersionFolder("shipping", "OrderShipped", 1)
+                            ))
+                    )),
+                    createNamespaceFolder("ordering", listOf(
+                            createEventFolder("ordering", "OrderReceived", listOf(
+                                    createEventVersionFolder("ordering", "OrderReceived", 1),
+                            )),
+                            createEventFolder("ordering", "OrderProcessed", listOf(
+                                    createEventVersionFolder("ordering", "OrderProcessed", 1),
+                            )),
+                            createEventFolder("ordering", "OrderProcessed", listOf(
+                                    createEventVersionFolder("ordering", "OrderProcessed", 2),
+                            ))
+                    )),
+            ))
+
+            val whiteList = listOf(
+                    "/shipping/**",
+                    "/ordering/Order*/versions/1"
+            )
+            val filteredProject = filterProject(project, whiteList)
+
+            filteredProject.namespaces[0].eventFolders[0].versionFolders.size shouldBe 1
+            filteredProject.namespaces[1].eventFolders.size shouldBe 2
+            filteredProject.namespaces[1].eventFolders[0].versionFolders.size shouldBe 1
+            filteredProject.namespaces[1].eventFolders[1].versionFolders.size shouldBe 1
+            filteredProject.namespaces[1].eventFolders[1].versionFolders[0].path.endsWith("OrderProcessed/versions/1") shouldBe true
+        }
     }
 
-    private fun filterProject(project: ProjectFolder, whiteList: List<String>, ): ProjectFolder {
+    private fun filterProject(project: ProjectFolder, whiteList: List<String>): ProjectFolder {
 
         var namespaces = mutableListOf<NamespaceFolder>()
-        project.copy().namespaces.forEach {nameSpaceFolder ->
+        project.copy().namespaces.forEach { nameSpaceFolder ->
 
             var eventFolders = mutableListOf<EventFolder>()
             nameSpaceFolder.eventFolders.forEach { eventFolder ->
 
                 var eventVersionFolders = mutableListOf<EventVersionFolder>()
                 eventFolder.versionFolders.forEach { eventVersionFolder ->
-                    if (whiteList.any{whiteListEntry -> eventVersionFolder.path.endsWith(whiteListEntry)}) {
+                    if (whiteList.any { whiteListEntry ->
+                                val globPattern = "glob:" + project.path.toString() + whiteListEntry
+                                val matcher = FileSystems.getDefault().getPathMatcher(globPattern)
+                                Paths.get(project.path.toString(), eventVersionFolder.path.toString())
+                                matcher.matches(eventVersionFolder.path)
+                            }) {
                         eventVersionFolders.add(eventVersionFolder)
                     }
                 }
+
+                if (eventVersionFolders.isEmpty()) return@forEach // if there is no version ignore the event completely
                 eventFolders.add(EventFolder(
                         eventFolder.path, eventVersionFolders, eventFolder.description, eventFolder.transformationFolders))
             }
@@ -83,11 +171,11 @@ class WhiteListFilterTest : StringSpec() {
                     eventFolders = eventFolders)
 
     private fun createEventFolder(namespaceName: String, eventName: String, versionFolders: List<EventVersionFolder>) =
-        EventFolder(
-                path = Paths.get("/registry/${namespaceName}/${eventName}"),
-                description = Paths.get("/registry/${namespaceName}/${eventName}/index.md"),
-                transformationFolders = emptyList(),
-                versionFolders = versionFolders)
+            EventFolder(
+                    path = Paths.get("/registry/${namespaceName}/${eventName}"),
+                    description = Paths.get("/registry/${namespaceName}/${eventName}/index.md"),
+                    transformationFolders = emptyList(),
+                    versionFolders = versionFolders)
 
     private fun createEventVersionFolder(namespaceName: String, eventName: String, version: Int = 1) =
             EventVersionFolder(
