24101a3ddffa6ed7cd7200c160d6789dc7417ac1
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-11-26 18:15:00 +0100

    #1092_whitelist: --schema-strip-titles command line argument

--- factcast-schema-registry-cli/src/main/kotlin/org/factcast/schema/registry/cli/registry/impl/DistributionCreatorServiceImpl.kt
+++ factcast-schema-registry-cli/src/main/kotlin/org/factcast/schema/registry/cli/registry/impl/DistributionCreatorServiceImpl.kt
@@ -28,10 +28,10 @@ class DistributionCreatorServiceImpl(
     private val staticPageCreator: StaticPageCreator,
     private val factcastIndexCreator: FactcastIndexCreator
 ) : DistributionCreatorService {
-    override fun createDistributable(outputPath: Path, project: Project) {
+    override fun createDistributable(outputPath: Path, project: Project, schemaStripTitles: Boolean) {
         staticPageCreator.createPage(outputPath, project)
 
         val indexPath = outputPath.resolve(Paths.get("static", "registry"))
-        factcastIndexCreator.createFactcastIndex(indexPath, project)
+        factcastIndexCreator.createFactcastIndex(indexPath, project, schemaStripTitles)
     }
 }
