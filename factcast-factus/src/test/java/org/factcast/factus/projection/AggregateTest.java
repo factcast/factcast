b010a435e1e257296ee1006facfefb4740bbb76e
Uwe Schaefer <uwe@uwe-schaefer.org>
2020-12-05 11:41:27 +0100

    unit test

---
+++ factcast-factus/src/test/java/org/factcast/factus/projection/AggregateTest.java
@@ -0,0 +1,45 @@
+package org.factcast.factus.projection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.val;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AggregateTest {
+
+  @Test
+  void hashOfId() {
+    val a = new TestAggregate();
+    UUID id = spy(UUID.randomUUID());
+    a.aggregateId(id);
+
+    val result = a.hashCode();
+    assertEquals(Objects.hash(id), result);
+  }
+
+  @Test
+  void equals() {
+    val a = new TestAggregate();
+    val b = new TestAggregate();
+    a.aggregateId(UUID.randomUUID());
+    b.aggregateId(UUID.randomUUID());
+
+    assertNotEquals(a, b);
+    assertNotEquals(b, a);
+
+    b.aggregateId(a.aggregateId());
+
+    assertEquals(a, b);
+    assertEquals(b, a);
+  }
+
+  class TestAggregate extends Aggregate {}
+}
