0d78263c5a0763a2e4c21c36bfff36763c970879
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-02-15 22:40:38 +0100

    #1163: reset examples

--- factcast-examples/factcast-example-client-spring-boot2/src/main/java/org/factcast/example/client/spring/boot2/hello/HelloWorldRunner.java
+++ factcast-examples/factcast-example-client-spring-boot2/src/main/java/org/factcast/example/client/spring/boot2/hello/HelloWorldRunner.java
@@ -15,83 +15,52 @@
  */
 package org.factcast.example.client.spring.boot2.hello;
 
+import java.util.UUID;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.factcast.core.Fact;
 import org.factcast.core.FactCast;
-import org.factcast.core.store.FactStore;
-import org.factcast.factus.Factus;
+import org.factcast.core.spec.FactSpec;
+import org.factcast.core.subscription.SubscriptionRequest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-@Slf4j
 public class HelloWorldRunner implements CommandLineRunner {
 
   @NonNull private final FactCast fc;
 
-  @NonNull FactStore store;
-  @NonNull Factus factus;
-
   @Override
   public void run(String... args) throws Exception {
-    //
-    //    val id = UUID.randomUUID();
-    //    Fact fact =
-    //        Fact.builder()
-    //            .ns("users")
-    //            .type("UserCreated")
-    //            .version(1)
-    //            .id(id)
-    //            .build("{\"firstName\":\"Horst\",\"lastName\":\"Lichter\"}");
-    //    fc.publish(fact);
-    //
-    //    val uc = fc.fetchById(id);
-    //    System.out.println(uc.get().jsonPayload());
-    //
-    //    val uc1 = fc.fetchByIdAndVersion(id, 1);
-    //    System.out.println(uc1.get().jsonPayload());
-    //
-    //    val uc2 = fc.fetchByIdAndVersion(id, 2);
-    //    System.out.println(uc2.get().jsonPayload());
-    //
-    //    val uc3 = fc.fetchByIdAndVersion(id, 6);
 
-    //    val uc3 = fc.fetchByIdAndVersion(id, 3);
-    //    System.out.println(uc3.get().jsonPayload());
-    //
+    val id = UUID.randomUUID();
+    Fact fact =
+        Fact.builder()
+            .ns("users")
+            .type("UserCreated")
+            .version(1)
+            .id(id)
+            .build("{\"firstName\":\"Horst\",\"lastName\":\"Lichter\"}");
+    fc.publish(fact);
+    System.out.println("published " + fact);
 
-    //
-    //    // TODO tmp, to trigger event on metrics
-    //
-    //    SnapshotId sid = new SnapshotId("foo", UUID.randomUUID());
-    //    @NonNull
-    //    Snapshot snap =
-    //        new Snapshot(sid, UUID.randomUUID(), "foo".getBytes(StandardCharsets.UTF_8), true);
-    //    store.setSnapshot(snap);
-    //    store.getSnapshot(sid);
-    //    store.clearSnapshot(sid);
-    //    store.currentTime();
-    //    store.enumerateNamespaces();
-    //    store.enumerateTypes("foo");
+    val uc = fc.fetchById(id);
+    System.out.println(uc.get().jsonPayload());
 
-    //    CompletableFuture.runAsync(
-    //        () -> {
-    //          try {
-    //            Thread.sleep(1000);
-    //          } catch (InterruptedException e) {
-    //            e.printStackTrace();
-    //          }
-    //          log.warn("publishing UC from Thread 2");
-    //          factus.publish(new UserCreated("a", "b", "Mr", "d"));
-    //        });
-    //
-    factus
-        .withLockOn(UserNames.class)
-        .attempt(
-            (names, tx) -> {
-              tx.publish(new UserCreated("a", "b", "Mr", "d"));
-            });
+    val uc1 = fc.fetchByIdAndVersion(id, 1);
+    System.out.println(uc1.get().jsonPayload());
+
+    val uc2 = fc.fetchByIdAndVersion(id, 2);
+    System.out.println(uc2.get().jsonPayload());
+
+    val uc3 = fc.fetchByIdAndVersion(id, 3);
+    System.out.println(uc3.get().jsonPayload());
+
+    fc.subscribe(
+            SubscriptionRequest.catchup(FactSpec.ns("users")).fromScratch(),
+            element -> System.out.println(element))
+        .awaitCatchup();
   }
 }
