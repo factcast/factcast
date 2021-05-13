ad6701f33dbac15011f483256ff27bf8aa9b8229
Uwe Schaefer <uwe@uwe-schaefer.org>
2021-05-13 14:53:21 +0200

    suppress warnings convenience methods

--- factcast-factus/src/main/java/org/factcast/factus/SuppressFactusWarnings.java
+++ factcast-factus/src/main/java/org/factcast/factus/SuppressFactusWarnings.java
@@ -4,7 +4,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import lombok.NonNull;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
@@ -20,6 +23,20 @@ public @interface SuppressFactusWarnings {
     public boolean isSuppressed(SuppressFactusWarnings annotation) {
       if (annotation == null) return false;
       else return Arrays.stream(annotation.warning()).anyMatch(w -> w == ALL || w == this);
+    }
+
+    public boolean isSuppressedOn(@NonNull Class<?> c) {
+      return isSuppressed(c.getAnnotation(SuppressFactusWarnings.class));
+    }
+
+    public boolean isSuppressedOn(@NonNull Method m) {
+      return isSuppressed(m.getAnnotation(SuppressFactusWarnings.class))
+          || isSuppressedOn(m.getDeclaringClass());
+    }
+
+    public boolean isSuppressedOn(@NonNull Field f) {
+      return isSuppressed(f.getAnnotation(SuppressFactusWarnings.class))
+          || isSuppressedOn(f.getDeclaringClass());
     }
   }
 }
