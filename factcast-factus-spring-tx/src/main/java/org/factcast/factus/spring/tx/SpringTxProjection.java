8986c92408501db513507b0e3e94eeef475d755e
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-06-21 17:46:44 +0200

    #1148: minor polishing

--- factcast-factus-spring-tx/src/main/java/org/factcast/factus/spring/tx/SpringTxProjection.java
+++ factcast-factus-spring-tx/src/main/java/org/factcast/factus/spring/tx/SpringTxProjection.java
@@ -3,6 +3,7 @@ package org.factcast.factus.spring.tx;
 import org.factcast.factus.projection.Projection;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@SpringTransactional
 public interface SpringTxProjection extends Projection {
   PlatformTransactionManager platformTransactionManager();
 }
--- factcast-factus-spring-tx/src/main/java/org/factcast/factus/spring/tx/SpringTxProjectorPlugin.java
+++ factcast-factus-spring-tx/src/main/java/org/factcast/factus/spring/tx/SpringTxProjectorPlugin.java
@@ -3,6 +3,7 @@ package org.factcast.factus.spring.tx;
 import java.util.Collection;
 import java.util.Collections;
 import javax.annotation.Nullable;
+import lombok.NonNull;
 import org.factcast.factus.projection.Projection;
 import org.factcast.factus.projector.ProjectorLens;
 import org.factcast.factus.projector.ProjectorPlugin;
@@ -11,7 +12,7 @@ public class SpringTxProjectorPlugin implements ProjectorPlugin {
 
   @Nullable
   @Override
-  public Collection<ProjectorLens> lensFor(Projection p) {
+  public Collection<ProjectorLens> lensFor(@NonNull Projection p) {
     if (p instanceof SpringTxProjection) {
 
       SpringTransactional transactional = p.getClass().getAnnotation(SpringTransactional.class);
