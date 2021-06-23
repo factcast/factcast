723cd70727345e3b6d47dd3f44837c6a6b5526ec
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-06-23 14:31:24 +0200

    #1149_tail: review comments

--- factcast-store-pgsql/src/main/java/org/factcast/store/pgsql/internal/tail/PGTailIndexManager.java
+++ factcast-store-pgsql/src/main/java/org/factcast/store/pgsql/internal/tail/PGTailIndexManager.java
@@ -1,11 +1,7 @@
 package org.factcast.store.pgsql.internal.tail;
 
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.factcast.core.subscription.observer.FastForwardTarget;
-import org.springframework.scheduling.annotation.Scheduled;
 
 public interface PGTailIndexManager extends FastForwardTarget {
-  @Scheduled(cron = "* * */1 * * *")
-  @SchedulerLock(name = "triggerTailCreation", lockAtMostFor = "120m")
   void triggerTailCreation();
 }
