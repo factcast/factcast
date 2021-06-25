a19a612883b763826be7b0ae90545b7ef8fe224c
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-06-25 20:15:44 +0200

    #1332-excpetionhandling-tests: some coverage

---
+++ factcast-client-grpc/src/test/java/org/factcast/client/grpc/MissesRequiredContructorException.java
@@ -0,0 +1,10 @@
+package org.factcast.client.grpc;
+
+public class MissesRequiredContructorException extends RuntimeException {
+
+  private static final long serialVersionUID = 2419646008542020255L;
+
+  MissesRequiredContructorException(int i) {
+    super("");
+  }
+}
