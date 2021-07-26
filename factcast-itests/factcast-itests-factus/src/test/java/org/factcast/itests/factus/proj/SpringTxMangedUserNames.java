68c59f7b30c29b3876f58c824ffcdef5dd676fca
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2021-07-26 14:26:24 +0200

    simple example Spring Tx managed proj

---
+++ factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/proj/SpringTxMangedUserNames.java
@@ -0,0 +1,76 @@
+package org.factcast.itests.factus.proj;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.factcast.factus.Handler;
+import org.factcast.factus.projection.WriterToken;
+import org.factcast.factus.serializer.ProjectionMetaData;
+import org.factcast.factus.spring.tx.AbstractSpringTxManagedProjection;
+import org.factcast.factus.spring.tx.SpringTransactional;
+import org.factcast.itests.factus.event.UserCreated;
+import org.factcast.itests.factus.event.UserDeleted;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
+@ProjectionMetaData(serial = 1)
+@Slf4j
+@SpringTransactional
+public class SpringTxMangedUserNames extends AbstractSpringTxManagedProjection {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SpringTxMangedUserNames(
+            @NonNull PlatformTransactionManager platformTransactionManager, JdbcTemplate jdbcTemplate) {
+        super(platformTransactionManager);
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<String> getUserNames() {
+        return jdbcTemplate.query("SELECT name FROM users", (rs, rowNum) -> rs.getString(1));
+    }
+
+    @Handler
+    void apply(UserCreated e) {
+        log.info("received event: " + e);
+        jdbcTemplate.update(
+                "INSERT INTO users (name, id) VALUES (?,?);", e.userName(), e.aggregateId());
+    }
+
+    @Handler
+    void apply(UserDeleted e) {
+        log.info("received event: " + e);
+        jdbcTemplate.update("DELETE FROM users where id = ?", e.aggregateId());
+    }
+
+    @Override
+    public UUID state() {
+        try {
+            return jdbcTemplate.queryForObject(
+                    "SELECT state FROM managed_projection WHERE name = ?",
+                    UUID.class,
+                    getScopedName().asString());
+        } catch (IncorrectResultSizeDataAccessException e) {
+            // no state yet, just return null
+            return null;
+        }
+    }
+
+    @Override
+    public void state(@NonNull UUID state) {
+        jdbcTemplate.update(
+                "INSERT INTO managed_projection (name, state) VALUES (?, ?) ON CONFLICT (name) DO UPDATE SET state = ?",
+                getScopedName().asString(),
+                state,
+                state);
+    }
+
+    @Override
+    public WriterToken acquireWriteToken(@NonNull Duration maxWait) {
+        return () -> { };
+    }
+}
