ee3d5851635b3877fcec3d1d26d2465c3e1164bc
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-07-30 09:22:12 +0200

    #1454: missing test

--- factcast-store-pgsql/src/test/java/org/factcast/store/pgsql/internal/check/IndexCheckTest.java
+++ factcast-store-pgsql/src/test/java/org/factcast/store/pgsql/internal/check/IndexCheckTest.java
@@ -3,6 +3,7 @@ package org.factcast.store.pgsql.internal.check;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Collections;
 import org.assertj.core.util.Lists;
 import org.factcast.test.Slf4jHelper;
 import org.junit.jupiter.api.*;
@@ -22,13 +23,14 @@ class IndexCheckTest {
   @Nested
   class WhenCheckingIndexes {
     @BeforeEach
-    void setup() {
-      when(jdbc.queryForList(any(), same(String.class)))
-          .thenReturn(Lists.newArrayList("idx1", "idx2"));
-    }
+    void setup() {}
 
     @Test
     void logsGivenIdexes() {
+
+      when(jdbc.queryForList(any(), same(String.class)))
+          .thenReturn(Lists.newArrayList("idx1", "idx2"));
+
       TestLogger logger = Slf4jHelper.replaceLogger(underTest);
       underTest.checkIndexes();
 
@@ -36,6 +38,17 @@ class IndexCheckTest {
       logger.contains("Detected invalid index: idx2");
       assertThat(logger.lines().size()).isEqualTo(2);
       assertThat(logger.lines().stream().allMatch(l -> l.level == LogLevel.WarnLevel)).isTrue();
+    }
+
+    @Test
+    void doesNotLogSilentIfNoInvalidIndexesFound() {
+
+      when(jdbc.queryForList(any(), same(String.class))).thenReturn(Collections.emptyList());
+
+      TestLogger logger = Slf4jHelper.replaceLogger(underTest);
+      underTest.checkIndexes();
+
+      assertThat(logger.lines()).isEmpty();
     }
   }
 }
