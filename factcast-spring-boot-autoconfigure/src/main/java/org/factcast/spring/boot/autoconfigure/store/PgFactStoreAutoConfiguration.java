84fb1af4c604ed824a014adcf7d95ee21bbb3309
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-08-02 15:04:53 +0200

    #638: renamed autoconf as well

--- factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/store/PgFactStoreAutoConfiguration.java
+++ factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/store/PgFactStoreAutoConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.factcast.spring.boot.autoconfigure.store.pgsql;
+package org.factcast.spring.boot.autoconfigure.store;
 
 import org.factcast.store.PgFactStoreConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
