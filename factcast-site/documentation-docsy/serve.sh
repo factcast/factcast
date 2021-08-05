787f0772b6e3551b8ca83b333dbab672cbba7d87
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-08-05 13:55:20 +0200

    server

--- factcast-site/documentation-docsy/serve.sh
+++ factcast-site/documentation-docsy/serve.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#(
-#docker run --rm -u `id -u`:`id -g` -p 1313:1313 -v $PWD:/srv/hugo peaceiris/hugo hugo server -w --bind 0.0.0.0
-#)
+(
+docker run --rm -u `id -u`:`id -g` -p 1313:1313 -v $PWD/../..:/srv/hugo factcast/factcast-hugo hugo server -s factcast-site/documentation-docsy -w --bind 0.0.0.0
+)
 
-
-hugo serve
