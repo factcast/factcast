0cecc6b772111510be1f9881307738e97209607a
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-08-06 09:45:06 +0200

    urls

--- factcast-site/documentation-docsy/build.sh
+++ factcast-site/documentation-docsy/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 (
-docker run --rm -u `id -u`:`id -g` -p 1313:1313 -v $PWD/../..:/srv/hugo factcast/factcast-hugo hugo -s factcast-site/documentation-docsy 
+docker run --rm -u `id -u`:`id -g` -p 1313:1313 -v $PWD/../..:/srv/hugo factcast/factcast-hugo hugo -s factcast-site/documentation-docsy --minify --debug --log
 )
 
