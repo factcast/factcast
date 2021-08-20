87149efce9d857ec9897fbfc4e7ed7eb54f26ec8
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-08-20 18:14:26 +0200

    favicons

--- factcast-site/documentation-docsy/release.sh
+++ factcast-site/documentation-docsy/release.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# example schema registry
+rm -rf static
+mkdir -p static
+cp -r ../../factcast-schema-registry-cli/output/public/ static/example-registry
+cp -r favicons static
+
+
 (
 docker run --rm -u `id -u`:`id -g` -v $PWD/../..:/srv/hugo factcast/factcast-hugo hugo -s factcast-site/documentation-docsy --minify --templateMetrics --buildDrafts
 )
 
-
-# example schema registry
-mkdir -p static
-cp -r ../../factcast-schema-registry-cli/output/public/ static/example-registry
 
 rsync --delete -rcv public/* con:/www/docs.factcast.org
