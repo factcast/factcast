86ea100146df93f151c7391f301cdcd34a0c6af2
Bernhard Streit (Extern) <Bernhard.Streit.Ext@prisma-capacity.eu>
2021-08-25 17:44:02 +0200

    #-1544-exception-on-tail-index-creation: fixed integration test setup

--- factcast-store/src/test/resources/test_schema.sql
+++ factcast-store/src/test/resources/test_schema.sql
@@ -11,100 +11,118 @@ DROP TABLE IF EXISTS snapshot_cache cascade;
 DROP TABLE IF EXISTS transformationstore cascade;
 DROP TABLE IF EXISTS transformationcache cascade;
 
-CREATE TABLE fact (
- ser SERIAL PRIMARY KEY,
- 
- header JSONB NOT NULL,
- payload JSONB NOT NULL
+CREATE TABLE fact
+(
+    ser     SERIAL PRIMARY KEY,
+
+    header  JSONB NOT NULL,
+    payload JSONB NOT NULL
 
 );
 
 #
 
-CREATE OR REPLACE FUNCTION notifyFactInsert() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION notifyFactInsert() RETURNS trigger AS
+$$
 BEGIN
-  PERFORM pg_notify('fact_insert', json_build_object(
-    'ser', NEW.ser,
-    'header',NEW.header
-  )::text);
-  RETURN NEW;
+    PERFORM pg_notify('fact_insert', json_build_object(
+            'ser', NEW.ser,
+            'header', NEW.header
+        )::text);
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 #
-CREATE CONSTRAINT TRIGGER tr_deferred_fact_insert AFTER INSERT ON fact DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE notifyFactInsert();
+CREATE CONSTRAINT TRIGGER tr_deferred_fact_insert
+    AFTER INSERT
+    ON fact DEFERRABLE INITIALLY DEFERRED
+    FOR EACH ROW
+EXECUTE PROCEDURE notifyFactInsert();
 
 #
 create sequence catchup_seq;
-create table catchup (
- cid bigint, 
- ser bigint, 
- ts timestamp
-); 
+create table catchup
+(
+    cid bigint,
+    ser bigint,
+    ts  timestamp
+);
 
 #
 
-CREATE TABLE IF NOT EXISTS tokenstore (
-	token 	UUID 		PRIMARY KEY 		DEFAULT uuid_generate_v4(),
-	ns	 	varchar 	,
-	state 	JSONB 		NOT NULL,
-	ts	 	TIMESTAMP 						DEFAULT now()
+CREATE TABLE IF NOT EXISTS tokenstore
+(
+    token UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    ns    varchar,
+    state JSONB NOT NULL,
+    ts    TIMESTAMP        DEFAULT now()
 );
 
- CREATE TABLE IF NOT EXISTS schemastore(
-	id 				varchar(2048)  PRIMARY KEY, 
-	hash 			varchar(32),
-	ns 				varchar(255),
-	type 			varchar(255),
-	version 		int,
-	jsonschema 		text,
-	UNIQUE(id),
-	UNIQUE(ns,type,version)	
+CREATE TABLE IF NOT EXISTS schemastore
+(
+    id         varchar(2048) PRIMARY KEY,
+    hash       varchar(32),
+    ns         varchar(255),
+    type       varchar(255),
+    version    int,
+    jsonschema text,
+    UNIQUE (id),
+    UNIQUE (ns, type, version)
 );
 
-CREATE TABLE transformationstore(
-    id 				varchar(2048) PRIMARY KEY,
-    hash 			varchar(32),
-    ns 				varchar(255) NOT NULL,
-    type 			varchar(255) NOT NULL,
-    from_version 	int NOT NULL,
-    to_version       int NOT NULL,
-    transformation 	text
+CREATE TABLE transformationstore
+(
+    id             varchar(2048) PRIMARY KEY,
+    hash           varchar(32),
+    ns             varchar(255) NOT NULL,
+    type           varchar(255) NOT NULL,
+    from_version   int          NOT NULL,
+    to_version     int          NOT NULL,
+    transformation text
 );
 
- CREATE TABLE IF NOT EXISTS transformationcache(
-	cache_key 		varchar(2048) PRIMARY KEY,
-	header 			text NOT NULL,
-	payload 		text NOT NULL,
-	last_access 		TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+CREATE TABLE IF NOT EXISTS transformationcache
+(
+    cache_key   varchar(2048) PRIMARY KEY,
+    header      text                                   NOT NULL,
+    payload     text                                   NOT NULL,
+    last_access TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS shedlock(name VARCHAR(64) NOT NULL, lock_until TIMESTAMP NOT NULL,
-    locked_at TIMESTAMP NOT NULL, locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name));
+CREATE TABLE IF NOT EXISTS shedlock
+(
+    name       VARCHAR(64)  NOT NULL,
+    lock_until TIMESTAMP    NOT NULL,
+    locked_at  TIMESTAMP    NOT NULL,
+    locked_by  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (name)
+);
 
-create table IF NOT EXISTS snapshot_cache (
-	cache_key 		varchar(2048) not null,
+create table IF NOT EXISTS snapshot_cache
+(
+    cache_key   varchar(2048)                          not null,
 
 -- note that the UUID is part of the primary key, so it is not nullable.
 -- while it makes sense to use a separate uuid when storing aggregate states, it is
 -- completely useless with projection states. In this case we store a fake uuid(0,0) in order to maintain the PK constraint
 
-	uuid     		uuid not null,
+    uuid        uuid                                   not null,
 
 -- represents the state of the blob (processed all relevant facts up to factid)
 
-	factid          uuid not null,
+    factid      uuid                                   not null,
 
-	data 			bytea not null,
+    data        bytea                                  not null,
 
 -- indicated if the data is already compressed in order to bypass transport compression if possible
 
-	compressed      boolean not null,
+    compressed  boolean                                not null,
 
-	last_access     timestamp with time zone default now() not null,
-	created_at    	timestamp with time zone default now() not null,
+    last_access timestamp with time zone default now() not null,
+    created_at  timestamp with time zone default now() not null,
 
-    primary key (uuid,cache_key)
+    primary key (uuid, cache_key)
 );
 
 
@@ -121,3 +139,30 @@ CREATE INDEX transformationcache_last_access ON public.transformationcache USING
 CREATE INDEX snapshot_cache_last_access ON snapshot_cache USING BTREE (last_access);
 
 
+DROP VIEW IF EXISTS stats_index;
+CREATE VIEW stats_index AS
+SELECT t.schemaname,
+       t.tablename,
+       c.reltuples::bigint                            AS num_rows,
+       pg_size_pretty(pg_relation_size(c.oid))        AS table_size,
+       psai.indexrelname                              AS index_name,
+       pg_size_pretty(pg_relation_size(i.indexrelid)) AS index_size,
+       CASE WHEN i.indisunique THEN 'Y' ELSE 'N' END  AS "unique",
+       psai.idx_scan                                  AS number_of_scans,
+       psai.idx_tup_read                              AS tuples_read,
+       psai.idx_tup_fetch                             AS tuples_fetched,
+       CASE
+           WHEN i.indisvalid THEN 'Y'
+           ELSE
+               CASE
+                   WHEN i is null THEN '-'
+                   ELSE
+                       'N' END
+           END
+                                                      AS "valid"
+FROM pg_tables t
+         LEFT JOIN pg_class c ON t.tablename = c.relname
+         LEFT JOIN pg_index i ON c.oid = i.indrelid
+         LEFT JOIN pg_stat_all_indexes psai ON i.indexrelid = psai.indexrelid
+WHERE t.schemaname NOT IN ('pg_catalog', 'information_schema')
+ORDER BY 1, 2;
