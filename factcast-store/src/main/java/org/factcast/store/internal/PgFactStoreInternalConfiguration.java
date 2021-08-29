1f76b7691cda5ef93804875e6a863b6636655f9c
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2021-08-29 18:02:24 +0200

    use method proxy method for shedlock

--- factcast-store/src/main/java/org/factcast/store/internal/PgFactStoreInternalConfiguration.java
+++ factcast-store/src/main/java/org/factcast/store/internal/PgFactStoreInternalConfiguration.java
@@ -71,7 +71,8 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock.InterceptMo
 @Configuration
 @EnableTransactionManagement
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT30m", interceptMode = InterceptMode.PROXY_SCHEDULER)
+// not that InterceptMode.PROXY_SCHEDULER does not work when wrapped at runtime (by opentelemetry for instance)
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30m", interceptMode = InterceptMode.PROXY_METHOD)
 @Import({
   SchemaRegistryConfiguration.class,
   PgSnapshotCacheConfiguration.class,
