b75ba8fd62113ddfb52a089a97be2205e6933526
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2021-09-20 15:09:24 +0200

    #s1160_doc_integration_test: #1591 unit test

---
+++ factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/UserEmailProjection.java
@@ -0,0 +1,32 @@
+package org.factcast.itests.docexample;
+
+import lombok.Getter;
+import org.factcast.factus.Handler;
+import org.factcast.factus.projection.LocalManagedProjection;
+import org.factcast.itests.docexample.event.UserAdded;
+import org.factcast.itests.docexample.event.UserEmailChanged;
+import org.factcast.itests.docexample.event.UserRemoved;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserEmailProjection extends LocalManagedProjection {
+
+    @Getter
+    private Set<String> userEmails = new HashSet<>();
+
+    @Handler
+    void apply(UserAdded event) {
+        userEmails.add(event.getEmail());
+    }
+
+    @Handler
+    void apply(UserEmailChanged event) {
+        userEmails.add(event.getEmail());
+    }
+
+    @Handler
+    void apply(UserRemoved event) {
+        userEmails.remove(event.getEmail());
+    }
+}
