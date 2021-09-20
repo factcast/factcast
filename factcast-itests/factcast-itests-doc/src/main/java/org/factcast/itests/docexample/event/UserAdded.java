aa8a211a062d08fb928b3717a7a1ba018ad2e655
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2021-09-20 19:24:43 +0200

    #s1160_doc_integration_test: int test works

--- factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/event/UserAdded.java
+++ factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/event/UserAdded.java
@@ -1,17 +1,29 @@
 package org.factcast.itests.docexample.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.factcast.factus.event.EventObject;
+import org.factcast.factus.event.Specification;
 
+import java.beans.ConstructorProperties;
 import java.util.Collections;
 import java.util.Set;
 import java.util.UUID;
 
 @Data
+@Specification(ns = "user", type = "UserAdded", version = 1)
 public class UserAdded implements EventObject {
 
-    private UUID userId;
-    private String email;
+    private final UUID userId;
+    private final String email;
+
+    // hint Jackson deserializer
+    @ConstructorProperties({"userId","email"})
+    public UserAdded(UUID userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
 
     @Override
     public Set<UUID> aggregateIds() {
--- factcast-itests/factcast-itests-doc/src/test/java/org/factcast/itests/docexample/factus/FactusExampleIntegrationTest.java
+++ factcast-itests/factcast-itests-doc/src/test/java/org/factcast/itests/docexample/factus/FactusExampleIntegrationTest.java
@@ -1,64 +1,62 @@
 package org.factcast.itests.docexample.factus;
 
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.factcast.factus.Factus;
-import org.factcast.factus.event.EventObject;
 import org.factcast.itests.docexample.UserEmailsProjection;
 import org.factcast.itests.docexample.event.UserAdded;
+import org.factcast.itests.docexample.event.UserEmailChanged;
+import org.factcast.itests.docexample.event.UserRemoved;
 import org.factcast.test.FactCastExtension;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.Collections;
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
 
-//@SpringBootTest
-//@ContextConfiguration(classes = {Application.class})
+@SpringBootTest
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-////@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
-//@Testcontainers(disabledWithoutDocker = true)
-//@ExtendWith({FactCastExtension.class})
-@Slf4j
+// provide fresh application context including uut for each test
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ExtendWith({FactCastExtension.class})
 public class FactusExampleIntegrationTest {
 
     @Autowired
     Factus factus;
 
-//    @Autowired
-//    UserEmailsProjection uut;
+    @Autowired
+    UserEmailsProjection uut;
 
-    @Disabled
     @Test
-    public void renameMe() {
-//        System.out.println("foo");
-//        factus.publish(new UserAdded().setEmail("foo@bar.com"));
-//        factus.update(uut);
-//        var emails = uut.getUserEmails();
-//        assertThat(emails).hasSize(2);
-//        assertThat(emails).containsExactly("foo@bar.com");
+    public void emailOfSingleUser() {
+        factus.publish(new UserAdded(UUID.randomUUID(), "user1@bar.com"));
+        factus.update(uut);
+        var emails = uut.getEmails();
+        assertThat(emails).hasSize(1);
+        assertThat(emails).containsExactly("user1@bar.com");
     }
 
     @Test
-    public void renameMe2() {
+    public void emailOfMultipleUsers() {
+        var userId1 = UUID.randomUUID();
+        var userId2 = UUID.randomUUID();
+        factus.publish(List.of(
+                new UserAdded(userId1, "user1@bar.com"),
+                new UserAdded(userId2, "user2@bar.com"),
+                new UserEmailChanged(userId1, "user1-new@bar.com"),
+                new UserRemoved(userId2)
+        ));
 
+        factus.update(uut);
+
+        var emails = uut.getEmails();
+        assertThat(emails).hasSize(1);
+        assertThat(emails).containsExactly("user1-new@bar.com");
     }
 
-    @Data
-    abstract private static class MinimalBaseEvent implements EventObject {
-        private String someProperty;
-
-        @Override
-        public Set<UUID> aggregateIds() {
-            return Collections.emptySet();
-        }
-    }
 }
