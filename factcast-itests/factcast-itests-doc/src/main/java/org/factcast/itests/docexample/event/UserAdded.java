a73201daef0c231346f2a8bb3b750344a38fa27c
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2021-09-20 15:09:24 +0200

    #s1160_doc_integration_test: #1591 unit test

---
+++ factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/event/UserAdded.java
@@ -0,0 +1,20 @@
+package org.factcast.itests.docexample.event;
+
+import lombok.Data;
+import org.factcast.factus.event.EventObject;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+public class UserAdded implements EventObject {
+
+    private UUID userId;
+    private String email;
+
+    @Override
+    public Set<UUID> aggregateIds() {
+        return Collections.emptySet();
+    }
+}
