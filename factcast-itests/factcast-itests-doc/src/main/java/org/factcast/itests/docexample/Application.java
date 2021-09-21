f7105bec442f3bf5515c023f91ceba628912f633
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2021-09-21 17:30:53 +0200

    #s1160_doc_integration_test: cleanup

--- factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/Application.java
+++ factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/Application.java
@@ -1,40 +1,14 @@
 package org.factcast.itests.docexample;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  public ObjectMapper objectMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.setVisibility(
-            mapper
-                    .getSerializationConfig()
-                    .getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
-                    .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    return mapper;
-  }
-
-  @Bean
-  public MeterRegistry meterRegistry() {
-    return new SimpleMeterRegistry();
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
