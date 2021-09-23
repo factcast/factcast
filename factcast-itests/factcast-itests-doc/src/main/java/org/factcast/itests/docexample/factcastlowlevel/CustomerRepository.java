f54d571bc734643745cdca747ee3bc443b9fdfeb
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2021-09-23 17:20:26 +0200

    #s1160_doc_integration_test: done lowlevel

--- factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/factcastlowlevel/CustomerRepository.java
+++ factcast-itests/factcast-itests-doc/src/main/java/org/factcast/itests/docexample/factcastlowlevel/CustomerRepository.java
@@ -8,10 +8,11 @@ import org.factcast.core.subscription.SubscriptionRequest;
 import org.factcast.core.subscription.observer.FactObserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
-@Component
+@Repository
 public class CustomerRepository {
 
     @Autowired
