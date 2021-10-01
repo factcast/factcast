19b17dd3ff1091d24ebf5044bf1de76102b4fe19
Benjamin Otto <benjamin.otto@style-systems.de>
2021-10-01 11:05:34 +0200

    #1619: failing test

--- factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/RedisTransactionalITest.java
+++ factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/RedisTransactionalITest.java
@@ -6,10 +6,13 @@ import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.factcast.factus.Factus;
+import org.factcast.factus.Handler;
 import org.factcast.factus.event.EventObject;
+import org.factcast.factus.redis.AbstractRedisManagedProjection;
 import org.factcast.factus.redis.tx.RedisTransactional;
 import org.factcast.factus.serializer.ProjectionMetaData;
 import org.factcast.itests.factus.event.UserCreated;
+import org.factcast.itests.factus.event.UserDeleted;
 import org.factcast.itests.factus.proj.TxRedissonManagedUserNames;
 import org.factcast.itests.factus.proj.TxRedissonSubscribedUserNames;
 import org.factcast.test.AbstractFactCastIntegrationTest;
@@ -28,10 +31,12 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static java.util.UUID.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ContextConfiguration(classes = {Application.class, RedissonProjectionConfiguration.class})
@@ -168,6 +173,19 @@ public class RedisTransactionalITest extends AbstractFactCastIntegrationTest {
     }
   }
 
+  @Nested
+  class TxMultipleHandlers {
+    @Test
+    void test() {
+      final var userId = randomUUID();
+
+      factus.publish(List.of(new UserCreated(userId, "hugo"), new UserDeleted(userId)));
+
+      final var proj = new TxMultipleHandler(redissonClient);
+      assertDoesNotThrow(() -> factus.update(proj));
+    }
+  }
+
   static class TrackingTxRedissonManagedUserNames extends TxRedissonManagedUserNames {
     public TrackingTxRedissonManagedUserNames(RedissonClient redisson) {
       super(redisson);
@@ -296,6 +314,26 @@ public class RedisTransactionalITest extends AbstractFactCastIntegrationTest {
         throw new IllegalStateException("Bad luck");
       }
       super.apply(created, tx);
+    }
+  }
+
+  @ProjectionMetaData(serial = 1)
+  @RedisTransactional
+  static class TxMultipleHandler extends AbstractRedisManagedProjection {
+    public TxMultipleHandler(RedissonClient redisson) {
+      super(redisson);
+    }
+
+    @Handler
+    void apply(UserCreated e, RTransaction tx) {
+      tx.getSet("hugo").add(e.aggregateId());
+    }
+
+    @Handler
+    void apply(UserDeleted e, RTransaction tx) {
+      if (!tx.getSet("hugo").contains(e.aggregateId())) {
+        throw new IllegalArgumentException("user should be in map but wasnt");
+      }
     }
   }
 }
