b80976fe78e7abad0722971d635b4286ebe27530
Benjamin Otto <benjamin.otto@style-systems.de>
2022-02-02 18:39:04 +0100

    #1785: fix filter

--- factcast-schema-registry-cli/src/test/kotlin/org/factcast/schema/registry/cli/utils/FilterJsonTest.kt
+++ factcast-schema-registry-cli/src/test/kotlin/org/factcast/schema/registry/cli/utils/FilterJsonTest.kt
@@ -1,6 +1,7 @@
 package org.factcast.schema.registry.cli.utils
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.karumi.kotlinsnapshot.matchWithSnapshot
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -13,31 +14,83 @@ class FilterJsonTest : StringSpec() {
 
             val inputJson = """
                 {
-                    "foo" : "bar",
-                    "title" : "i am unwanted",
+                    "foo" : {
+                      "title" : "i am unwanted 1",
+                      "type": "string",
+                      "example": "foo"
+                    },
                     "nested" : {
-                        "foo" : "bar",
-                        "title" : "i am unwanted",
-                        "example": "foo"
+                        "type": "object",
+                        "description": "bar",
+                        "properties": {
+                          "foo": {
+                            "type": "string",
+                            "title" : "i am unwanted 2"                            
+                          }
+                        }
+                    },
+                    "type": {
+                       "title" : "i am unwanted 3",
+                       "type": "string",
+                       "example": "foo"
                     }
                 }
             """.trimIndent()
             val unfiltered = objectMapper.readTree(inputJson)
 
-            val filtered = filterJson(unfiltered, setOf("title", "example"))
+            val filtered = filterJson(unfiltered, setOf("title", "example", "description"))
 
+            filtered.toPrettyString().matchWithSnapshot("filter-complex")
             filtered.findParents("title")?.size shouldBe 0
             filtered.findParents("example")?.size shouldBe 0
             filtered.findParents("foo")?.size shouldBe 2
+            filtered.findParents("nested")?.size shouldBe 1
+            filtered.findParents("type")?.size shouldBe 4
+        }
+
+        "filters oneOf" {
+
+            val inputJson = """
+                {
+                     "oneOf" : [ {
+                        "title" : "TransactionType(deal) / MetaData(meta)",
+                        "required" : [ "transportAuctionAllocationId", "meta" ]
+                      } ]
+                }
+            """.trimIndent()
+            val unfiltered = objectMapper.readTree(inputJson)
+
+            val filtered = filterJson(unfiltered, setOf("title"))
+
+            filtered.toPrettyString().matchWithSnapshot("filter-oneof")
+            filtered.findParents("title")?.size shouldBe 0
+            filtered.findParents("required")?.size shouldBe 1
+        }
+
+
+        "does not remove a property called 'title'" {
+
+            val inputJson = """
+                  {
+                    "title": {
+                      "title": "i am unwanted",
+                      "type": "string"
+                    }
+                }
+            """.trimIndent()
+            val unfiltered = objectMapper.readTree(inputJson)
+
+            val filtered = filterJson(unfiltered, setOf("title"))
+
+            filtered.toPrettyString().matchWithSnapshot("filter-should-not-filter-title-property")
         }
 
         "no content change if title is missing" {
             // arrange
             val inputJson = """
                 {
-                    "foo" : "bar",
-                    "nested" : {
-                        "foo" : "bar"
+                    "foo" : {
+                      "type": "string"
                     }
                 }
             """.trimIndent()
