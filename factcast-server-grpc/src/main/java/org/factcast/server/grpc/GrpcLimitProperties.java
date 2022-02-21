ec1598933c3bdb955cce8ef90184c17753b5efcd
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-02-21 22:31:45 +0100

    Revert "unrelated cleanup (boyscout)"
    This reverts commit ecf4faaaa3a4bd6d623c9341ec48c98bb03d6d82.

--- factcast-server-grpc/src/main/java/org/factcast/server/grpc/GrpcLimitProperties.java
+++ factcast-server-grpc/src/main/java/org/factcast/server/grpc/GrpcLimitProperties.java
@@ -16,15 +16,12 @@
 package org.factcast.server.grpc;
 
 import lombok.Data;
-import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "factcast.grpc.bandwidth")
 @Data
-// Spring needs classic setters
-@Accessors(fluent = false, chain = false)
 public class GrpcLimitProperties {
 
   private int initialNumberOfFollowRequestsAllowedPerClient = 50;
@@ -36,4 +33,33 @@ public class GrpcLimitProperties {
   private int numberOfCatchupRequestsAllowedPerClientPerMinute = 6000;
 
   private boolean disabled = false;
+
+  // Spring needs classic setters
+  public void setInitialNumberOfFollowRequestsAllowedPerClient(
+      int initialNumberOfFollowRequestsAllowedPerClient) {
+    this.initialNumberOfFollowRequestsAllowedPerClient =
+        initialNumberOfFollowRequestsAllowedPerClient;
+  }
+
+  public void setNumberOfFollowRequestsAllowedPerClientPerMinute(
+      int numberOfFollowRequestsAllowedPerClientPerMinute) {
+    this.numberOfFollowRequestsAllowedPerClientPerMinute =
+        numberOfFollowRequestsAllowedPerClientPerMinute;
+  }
+
+  public void setInitialNumberOfCatchupRequestsAllowedPerClient(
+      int initialNumberOfCatchupRequestsAllowedPerClient) {
+    this.initialNumberOfCatchupRequestsAllowedPerClient =
+        initialNumberOfCatchupRequestsAllowedPerClient;
+  }
+
+  public void setNumberOfCatchupRequestsAllowedPerClientPerMinute(
+      int numberOfCatchupRequestsAllowedPerClientPerMinute) {
+    this.numberOfCatchupRequestsAllowedPerClientPerMinute =
+        numberOfCatchupRequestsAllowedPerClientPerMinute;
+  }
+
+  public void setDisabled(boolean disabled) {
+    this.disabled = disabled;
+  }
 }
