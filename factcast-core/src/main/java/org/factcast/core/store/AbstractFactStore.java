b1418cc9c622f97639fd085f0c014d03dcb20f7e
Mattia Brescia <9354343-mattiabrescia@users.noreply.gitlab.com>
2022-02-22 11:54:40 +0100

    #1823 - call own method for invalidation

--- factcast-core/src/main/java/org/factcast/core/store/AbstractFactStore.java
+++ factcast-core/src/main/java/org/factcast/core/store/AbstractFactStore.java
@@ -43,7 +43,7 @@ public abstract class AbstractFactStore implements FactStore {
             return false;
           }
         } finally {
-          tokenStore.invalidate(token);
+          invalidate(token);
         }
       } else {
         // token is unknown, just reject.
