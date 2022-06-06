52df1b68666e393e81301ac7190904d29c2c343b
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-06-06 17:36:38 +0200

    #1834 added unit test

--- factcast-factus/src/main/java/org/factcast/factus/FactusImpl.java
+++ factcast-factus/src/main/java/org/factcast/factus/FactusImpl.java
@@ -91,7 +91,7 @@ public class FactusImpl implements Factus {
   private final Set<AutoCloseable> managedObjects = new HashSet<>();
 
   @Override
-  public PublishBatch batch() {
+  public @NonNull PublishBatch batch() {
     return new DefaultPublishBatch(fc, eventConverter);
   }
 
@@ -243,7 +243,7 @@ public class FactusImpl implements Factus {
 
   @Override
   @SneakyThrows
-  public <P extends SnapshotProjection> P fetch(Class<P> projectionClass) {
+  public <P extends SnapshotProjection> @NonNull P fetch(Class<P> projectionClass) {
     return factusMetrics.timed(
         TimedOperation.FETCH_DURATION,
         Tags.of(Tag.of(CLASS, projectionClass.getName())),
@@ -354,7 +354,7 @@ public class FactusImpl implements Factus {
 
     FactObserver fo =
         new AbstractFactObserver(projection, PROGRESS_INTERVAL, factusMetrics) {
-          @NonNull UUID id = null;
+          UUID id = null;
 
           @Override
           public void onNextFact(@NonNull Fact element) {
@@ -446,7 +446,7 @@ public class FactusImpl implements Factus {
   }
 
   @Override
-  public <M extends ManagedProjection> Locked<M> withLockOn(M managedProjection) {
+  public <M extends ManagedProjection> Locked<M> withLockOn(@NonNull M managedProjection) {
     Projector<M> applier = ehFactory.create(managedProjection);
     List<FactSpec> specs = applier.createFactSpecs();
     return new Locked<>(fc, this, managedProjection, specs, factusMetrics);
@@ -499,7 +499,7 @@ public class FactusImpl implements Factus {
     private final Duration duration;
     private Instant nextSnapshot;
 
-    public IntervalSnapshotter(Duration duration) {
+    IntervalSnapshotter(Duration duration) {
       this.duration = duration;
       nextSnapshot = Instant.now().plus(duration);
     }
