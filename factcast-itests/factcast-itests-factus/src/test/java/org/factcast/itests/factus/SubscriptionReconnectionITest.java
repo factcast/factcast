16115bcfef0d430b98c46125c82b8b6461405448
Mattia Brescia <9354343-mattiabrescia@users.noreply.gitlab.com>
2022-06-17 14:57:09 +0200

    #2047: refactor tests

--- factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/SubscriptionReconnectionITest.java
+++ factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/SubscriptionReconnectionITest.java
@@ -15,10 +15,6 @@
  */
 package org.factcast.itests.factus;
 
-import static java.util.concurrent.TimeUnit.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.awaitility.Awaitility.*;
-
 import com.google.common.base.Stopwatch;
 import eu.rekawek.toxiproxy.model.ToxicDirection;
 import io.grpc.StatusRuntimeException;
@@ -30,6 +26,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import nl.altindag.console.ConsoleCaptor;
 import org.factcast.core.Fact;
 import org.factcast.core.FactCast;
 import org.factcast.core.spec.FactSpec;
@@ -40,15 +37,20 @@ import org.factcast.core.subscription.SubscriptionRequest;
 import org.factcast.core.subscription.observer.FactObserver;
 import org.factcast.test.AbstractFactCastIntegrationTest;
 import org.factcast.test.FactCastExtension;
-import org.factcast.test.SLF4JTestSpy;
 import org.factcast.test.toxi.FactCastProxy;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @ContextConfiguration(classes = {Application.class})
@@ -153,8 +155,7 @@ class SubscriptionReconnectionITest extends AbstractFactCastIntegrationTest {
   @SneakyThrows
   @Test
   void subscribeWithFailingReconnect() {
-    try (var spy = SLF4JTestSpy.attach()) {
-
+    try (var consoleCaptor = new ConsoleCaptor()) {
       var count = new AtomicInteger();
       assertThatThrownBy(
               () ->
@@ -179,7 +180,8 @@ class SubscriptionReconnectionITest extends AbstractFactCastIntegrationTest {
       assertThat(count.get()).isLessThan(MAX_FACTS);
 
       assertThat(
-              spy.stream().filter(e -> e.getFormattedMessage().contains("Trying to resubscribe")))
+              consoleCaptor.getStandardOutput().stream()
+                  .filter(e -> e.contains("Trying to resubscribe")))
           .hasSize(NUMBER_OF_ATTEMPTS - 1);
     }
   }
