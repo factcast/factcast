929ec3980035d24a7692d0fe2a2f37428d079fd1
uweschaefer <uweschaefer@users.noreply.github.com>
2022-06-21 13:15:02 +0000

    Apply formatter

--- factcast-store/src/main/java/org/factcast/store/internal/PgFactStoreInternalConfiguration.java
+++ factcast-store/src/main/java/org/factcast/store/internal/PgFactStoreInternalConfiguration.java
@@ -15,10 +15,18 @@
  */
 package org.factcast.store.internal;
 
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import java.util.concurrent.*;
-
 import javax.sql.DataSource;
-
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock.InterceptMode;
 import org.factcast.core.store.FactStore;
 import org.factcast.core.store.TokenStore;
 import org.factcast.core.subscription.FactTransformerService;
@@ -51,20 +59,6 @@ import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.google.common.eventbus.AsyncEventBus;
-import com.google.common.eventbus.EventBus;
-
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-
-import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock.InterceptMode;
-
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Main @Configuration class for a PGFactStore
@@ -203,9 +197,7 @@ public class PgFactStoreInternalConfiguration {
     return new DataSourceTransactionManager(ds);
   }
 
-  /**
-   * @return A fallback {@code MeterRegistry} in case none is configured.
-   */
+  /** @return A fallback {@code MeterRegistry} in case none is configured. */
   @Bean
   @ConditionalOnMissingBean
   public MeterRegistry meterRegistry() {
