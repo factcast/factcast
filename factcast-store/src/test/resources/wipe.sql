6c46e28dc9b2cf3361ee071af5c7983b942f97f8
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-06-22 10:48:39 +0200

    fix flaky test

--- factcast-store/src/test/resources/wipe.sql
+++ factcast-store/src/test/resources/wipe.sql
@@ -3,9 +3,11 @@ truncate table blacklist restart identity;
 truncate table schemastore restart identity;
 truncate table transformationstore restart identity;
 truncate table transformationcache restart identity;
+truncate table snapshot_cache restart identity;
+truncate table tokenstore restart identity;
 
 
-
+select dropAllTailIndexes();
 
 
 
