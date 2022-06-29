0061549d5d4655f3dd0e032afa119b3a74391110
uweschaefer <uweschaefer@users.noreply.github.com>
2022-06-29 13:32:26 +0000

    Apply formatter

--- factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/RedisTransactionalITest.java
+++ factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/RedisTransactionalITest.java
@@ -17,9 +17,11 @@ package org.factcast.itests.factus;
 
 import static java.util.UUID.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import config.RedissonProjectionConfiguration;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NonNull;
@@ -49,14 +51,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import static java.util.UUID.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ContextConfiguration(classes = {Application.class, RedissonProjectionConfiguration.class})
