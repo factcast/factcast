9f3326c5c93d6236b2fd978c06e7aa2e94bfd1a7
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-07-01 11:07:11 +0200

    merge master

--- factcast-store/src/test/java/org/factcast/store/internal/PgFactStreamTest.java
+++ factcast-store/src/test/java/org/factcast/store/internal/PgFactStreamTest.java
@@ -15,18 +15,11 @@
  */
 package org.factcast.store.internal;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
-
-import com.google.common.eventbus.EventBus;
-import io.micrometer.core.instrument.DistributionSummary;
 import java.sql.ResultSet;
 import java.util.*;
 import java.util.concurrent.atomic.*;
 import java.util.function.*;
-import lombok.SneakyThrows;
+
 import org.factcast.core.subscription.SubscriptionImpl;
 import org.factcast.core.subscription.SubscriptionRequest;
 import org.factcast.core.subscription.SubscriptionRequestTO;
@@ -46,7 +39,18 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.google.common.eventbus.EventBus;
+
+import io.micrometer.core.instrument.DistributionSummary;
+
+import lombok.SneakyThrows;
 import slf4jtest.LogLevel;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PgFactStreamTest {
@@ -347,7 +351,7 @@ public class PgFactStreamTest {
       PgCatchup catchup1 = mock(PgCatchup.class);
       PgCatchup catchup2 = mock(PgCatchup.class);
       when(uut.isConnected()).thenReturn(true);
-      when(pgCatchupFactory.create(any(), any(), any(), any(), any()))
+      when(pgCatchupFactory.create(any(), any(), any(), any(), any(), any()))
           .thenReturn(catchup1, catchup2);
 
       uut.catchup(mock(PgPostQueryMatcher.class));
