566597695712bc6eb04d6663ac7d1074e6570f33
bresciamattia <bresciamattia@users.noreply.github.com>
2022-07-06 06:56:53 +0000

    Apply formatter

--- factcast-script-engine-graaljs/src/main/java/org/factcast/script/engine/graaljs/GraalJSEngineCache.java
+++ factcast-script-engine-graaljs/src/main/java/org/factcast/script/engine/graaljs/GraalJSEngineCache.java
@@ -1,4 +1,21 @@
+/*
+ * Copyright © 2017-2022 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.factcast.script.engine.graaljs;
+
+import static java.util.Collections.synchronizedMap;
 
 import java.util.Map;
 import lombok.NonNull;
@@ -7,8 +24,6 @@ import org.apache.commons.collections4.map.LRUMap;
 import org.factcast.script.engine.Engine;
 import org.factcast.script.engine.EngineCache;
 import org.factcast.script.engine.exception.ScriptEngineException;
-
-import static java.util.Collections.synchronizedMap;
 
 @Slf4j
 public class GraalJSEngineCache implements EngineCache {
