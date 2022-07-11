da22e6ac9f9dbaea95b2b58b8c480aab707161c5
Mattia Brescia <9354343-mattiabrescia@users.noreply.gitlab.com>
2022-07-11 12:42:33 +0200

    #2047: fix build after rebase and add engine factory to schema registry cli

--- factcast-schema-registry-cli/src/main/kotlin/org/factcast/schema/registry/cli/config/TransformerConfig.kt
+++ factcast-schema-registry-cli/src/main/kotlin/org/factcast/schema/registry/cli/config/TransformerConfig.kt
@@ -2,12 +2,17 @@ package org.factcast.schema.registry.cli.config
 
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
+import org.factcast.script.engine.EngineFactory
+import org.factcast.script.engine.graaljs.GraalJSEngineCache
 import org.factcast.store.registry.transformation.chains.GraalJsTransformer
 import org.factcast.store.registry.transformation.chains.Transformer
 
 @Factory
 class TransformerConfig {
     @Bean
-    fun transformer(): Transformer =
-        GraalJsTransformer()
+    fun engineFactory(): EngineFactory = GraalJSEngineCache()
+
+    @Bean
+    fun transformer(engineFactory: EngineFactory): Transformer =
+        GraalJsTransformer(engineFactory)
 }
