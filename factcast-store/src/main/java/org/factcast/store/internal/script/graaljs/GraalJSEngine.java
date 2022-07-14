e0e85bf13a351791db8f61ee25528e8e5332f83a
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-07-14 11:50:18 +0200

    fix graal truffle confusion

--- factcast-store/src/main/java/org/factcast/store/internal/script/graaljs/GraalJSEngine.java
+++ factcast-store/src/main/java/org/factcast/store/internal/script/graaljs/GraalJSEngine.java
@@ -15,15 +15,20 @@
  */
 package org.factcast.store.internal.script.graaljs;
 
-import com.oracle.truffle.api.Truffle;
-import com.oracle.truffle.js.scriptengine.GraalJSScriptEngine;
 import java.util.*;
 import java.util.function.*;
+
 import javax.script.ScriptException;
-import lombok.extern.slf4j.Slf4j;
+
 import org.factcast.store.internal.script.JSArgument;
 import org.factcast.store.internal.script.JSEngine;
 import org.factcast.store.internal.script.exception.ScriptEngineException;
+import org.factcast.store.registry.metrics.SupplierWithException;
+
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.js.scriptengine.GraalJSScriptEngine;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GraalJSEngine implements JSEngine {
@@ -33,20 +38,40 @@ public class GraalJSEngine implements JSEngine {
   private final GraalJSScriptEngine engine;
 
   GraalJSEngine(String script) throws ScriptEngineException {
-    try {
-      // not stated anywhere that engine creation is thread-safe
-      synchronized (ENGINE_CREATION_MUTEX) {
-        this.engine = GraalJSScriptEngine.create(null, NashornCompatContextBuilder.CTX);
-      }
-      this.engine.compile(script).eval();
-    } catch (RuntimeException | ScriptException e) {
-      log.debug("Exception during engine creation. Escalating.", e);
-      throw new ScriptEngineException(e);
-    }
+    this.engine =
+        withTruffleClassloader(
+            () -> {
+              try {
+                // not stated anywhere that engine creation is thread-safe
+                synchronized (ENGINE_CREATION_MUTEX) {
+                  var engine = GraalJSScriptEngine.create(null, NashornCompatContextBuilder.CTX);
+                  engine.compile(script).eval();
+                  return engine;
+                }
+              } catch (RuntimeException | ScriptException e) {
+                log.debug("Exception during engine creation. Escalating.", e);
+                throw new ScriptEngineException(e);
+              }
+            });
   }
 
   @Override
   public Object invoke(String functionName, JSArgument<?>... input) throws ScriptEngineException {
+    try {
+      synchronized (engine) {
+        return withTruffleClassloader(
+            () ->
+                engine.invokeFunction(
+                    functionName, Arrays.stream(input).map(Supplier::get).toArray()));
+      }
+    } catch (Exception e) {
+      log.debug("Exception during the invocation of '{}'. Escalating.", functionName, e);
+      throw new ScriptEngineException(e);
+    }
+  }
+
+  public synchronized <T, E extends Exception> T withTruffleClassloader(
+      SupplierWithException<T, E> block) {
     ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
     Thread.currentThread().setContextClassLoader(Truffle.class.getClassLoader());
     try {
@@ -55,12 +80,8 @@ public class GraalJSEngine implements JSEngine {
       // these Graal maps are bound to a Context which bound to a ScriptEngine in our case
       // jackson seems somehow to access fields of this map which leads to exceptions like
       // mentioned in https://github.com/factcast/factcast/issues/1506
-      synchronized (engine) {
-        return engine.invokeFunction(
-            functionName, Arrays.stream(input).map(Supplier::get).toArray());
-      }
-    } catch (RuntimeException | ScriptException | NoSuchMethodException e) {
-      log.debug("Exception during the invocation of '{}'. Escalating.", functionName, e);
+      return block.get();
+    } catch (Exception e) {
       throw new ScriptEngineException(e);
     } finally {
       Thread.currentThread().setContextClassLoader(oldCl);
