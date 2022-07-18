1083052025ea7381e531822097566024a5a17d05
uweschaefer <uweschaefer@users.noreply.github.com>
2022-07-18 12:04:32 +0000

    Apply formatter

--- factcast-store/src/test/java/org/factcast/store/internal/PgFactStoreTest.java
+++ factcast-store/src/test/java/org/factcast/store/internal/PgFactStoreTest.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright © 2017-2022 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.factcast.store.internal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 import java.util.*;
 import java.util.function.*;
-
+import lombok.NonNull;
 import org.assertj.core.util.Lists;
 import org.factcast.core.Fact;
 import org.factcast.core.snap.SnapshotId;
@@ -27,15 +45,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-import lombok.NonNull;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class PgFactStoreTest {
 
-  //TODO plenty of opportunities for unit tests
+  // TODO plenty of opportunities for unit tests
 
   @Mock private @NonNull JdbcTemplate jdbcTemplate;
   @Mock private @NonNull PgSubscriptionFactory subscriptionFactory;
