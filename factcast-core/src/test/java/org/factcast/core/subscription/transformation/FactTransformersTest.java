2c1c176c3fb8c57d51a3921a36d7b980f84c2049
uweschaefer <uweschaefer@users.noreply.github.com>
2022-07-18 10:08:14 +0000

    Apply formatter

--- factcast-core/src/test/java/org/factcast/core/subscription/transformation/FactTransformersTest.java
+++ factcast-core/src/test/java/org/factcast/core/subscription/transformation/FactTransformersTest.java
@@ -15,8 +15,11 @@
  */
 package org.factcast.core.subscription.transformation;
 
-import java.util.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
+import com.google.common.collect.Lists;
+import java.util.*;
 import org.factcast.core.Fact;
 import org.factcast.core.TestFact;
 import org.factcast.core.spec.FactSpec;
@@ -24,11 +27,6 @@ import org.factcast.core.subscription.SubscriptionRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.google.common.collect.Lists;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FactTransformersTest {
