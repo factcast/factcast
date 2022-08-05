7750010fc745e185eb8d15e3853bdd8051e2dc78
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-08-05 14:05:26 +0200

    #2119 fix starving liquibase

--- factcast-store/src/main/resources/db/changelog/factcast/issue1863/updateNotifyFactInsert_condensed_without_comment_corrected.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue1863/updateNotifyFactInsert_condensed_without_comment_corrected.sql
@@ -1,3 +1,6 @@
+-- looks like pg may starve if we're replacing a function used in a trigger that is busy.
+-- so even if we'd risk to loose a notify, we're now dropping the trigger first
+
 DROP TRIGGER IF EXISTS tr_deferred_fact_insert ON fact;
 CREATE OR REPLACE FUNCTION notifyFactInsert() RETURNS trigger AS
 
