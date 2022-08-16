b4fcbe7afdaea785cce76b0405ac0105a4206d14
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-08-16 18:02:58 +0200

    extract test phase and coverage sending from full build

--- .github/kts/maven-full.main.kts
+++ .github/kts/maven-full.main.kts
@@ -5,6 +5,7 @@
 import it.krzeminski.githubactions.actions.actions.CacheV3
 import it.krzeminski.githubactions.actions.actions.CheckoutV3
 import it.krzeminski.githubactions.actions.actions.SetupJavaV3
+import it.krzeminski.githubactions.actions.docker.LoginActionV2
 import it.krzeminski.githubactions.domain.RunnerType
 import it.krzeminski.githubactions.domain.Workflow
 import it.krzeminski.githubactions.domain.triggers.PullRequest
@@ -14,7 +15,7 @@ import it.krzeminski.githubactions.yaml.writeToFile
 import java.nio.file.Paths
 
 public val workflowMaven: Workflow = workflow(
-    name = "maven",
+    name = "maven-full",
     on = listOf(
         PullRequest(
             branches = listOf("master"),
@@ -23,12 +24,19 @@ public val workflowMaven: Workflow = workflow(
             branches = listOf("master"),
         ),
     ),
-    sourceFile = Paths.get(".github/kts/maven.main.kts"),
+    sourceFile = Paths.get(".github/kts/maven-full.main.kts"),
 ) {
     job(
         id = "build",
         runsOn = RunnerType.UbuntuLatest,
     ) {
+        uses(
+            name = "Login to Docker Hub",
+            action = LoginActionV2(
+                username = "${'$'}{{ secrets.DOCKERHUB_USERNAME }}",
+                password = "${'$'}{{ secrets.DOCKERHUB_TOKEN }}",
+            ),
+        )
         uses(
             name = "CheckoutV3",
             action = CheckoutV3(),
@@ -46,6 +54,18 @@ public val workflowMaven: Workflow = workflow(
             ),
         )
         uses(
+            name = "CacheV3",
+            action = CacheV3(
+                path = listOf(
+                    "/var/lib/docker/",
+                ),
+                key = "factcast-docker-cache-unversioned",
+                restoreKeys = listOf(
+                    "factcast-docker-cache-unversioned",
+                ),
+            ),
+        )
+        uses(
             name = "Set up JDK 11",
             action = SetupJavaV3(
                 distribution = SetupJavaV3.Distribution.Custom("corretto"),
@@ -54,10 +74,10 @@ public val workflowMaven: Workflow = workflow(
         )
         run(
             name = "Build with Maven",
-            command = "./mvnw -B clean test --file pom.xml",
+            command = "./mvnw -B clean verify --file pom.xml",
         )
     }
-}
 
+}
 
 workflowMaven.writeToFile(addConsistencyCheck = false)
