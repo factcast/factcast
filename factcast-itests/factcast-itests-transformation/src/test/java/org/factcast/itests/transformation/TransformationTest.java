02ccfdfdf3124d52f8efb476f491df056c4c2559
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-08-16 19:50:58 +0200

    unit test speedup and integration test tagging

--- factcast-itests/factcast-itests-transformation/src/test/java/org/factcast/itests/transformation/TransformationTest.java
+++ factcast-itests/factcast-itests-transformation/src/test/java/org/factcast/itests/transformation/TransformationTest.java
@@ -24,6 +24,7 @@ import org.factcast.core.FactCast;
 import org.factcast.core.FactValidationException;
 import org.factcast.core.subscription.transformation.MissingTransformationInformationException;
 import org.factcast.core.util.FactCastJson;
+import org.factcast.test.IntegrationTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
