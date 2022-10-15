d2d9fa9f9ffed623dac03daf784c7871b2ebf1f0
Benjamin Otto <benjamin.otto@style-systems.de>
2022-10-15 12:23:05 +0200

    #2168: rollback tx in case of statement cancellation

--- factcast-store/src/main/java/org/factcast/store/internal/query/CurrentStatementHolder.java
+++ factcast-store/src/main/java/org/factcast/store/internal/query/CurrentStatementHolder.java
@@ -33,7 +33,13 @@ public class CurrentStatementHolder implements Closeable {
       if (statement != null) {
         log.info("Canceling statement " + statement);
         try {
+
           statement.cancel();
+
+          // we have to rollback the tx on the undderlying transaction
+          // if we do not end the transaction, statements are cancelled but still "idle in
+          // transaction" and block further actions (like wiping between tests)
+          statement.getConnection().rollback();
           wasCanceled = true;
         } catch (SQLException e) {
           log.debug("Exception while closing statement {}:", statement, e);
