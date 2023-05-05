1917d26a7cac5d458bdce46daf752e562122c7b1
Benjamin Otto <benjamin.otto@style-systems.de>
2023-05-05 12:34:16 +0200

    #2276: closes #2276

--- factcast-store/src/main/java/org/factcast/store/registry/metrics/RegistryMetricsImpl.java
+++ factcast-store/src/main/java/org/factcast/store/registry/metrics/RegistryMetricsImpl.java
@@ -17,6 +17,8 @@ package org.factcast.store.registry.metrics;
 
 import com.google.common.base.Stopwatch;
 import io.micrometer.core.instrument.*;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
+import java.util.concurrent.ExecutorService;
 import java.util.function.*;
 import lombok.NonNull;
 import org.factcast.core.util.RunnableWithException;
@@ -130,6 +132,11 @@ public class RegistryMetricsImpl implements RegistryMetrics, InitializingBean {
   public void increase(@NonNull EVENT event, int amount) {
     Counter counter = counter(event, Tags.empty());
     counter.increment(amount);
+  }
+
+  @Override
+  public ExecutorService monitor(ExecutorService executor, String name) {
+    return ExecutorServiceMetrics.monitor(meterRegistry, executor, name);
   }
 
   @Override
