ed48ff26473d28ef93381f1619eb751c25194c68
Benjamin Otto <benjamin.otto@style-systems.de>
2023-05-10 11:35:40 +0200

    #2280: use different hugo image

--- factcast-site/documentation-docsy/serve.sh
+++ factcast-site/documentation-docsy/serve.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#(
-#docker run --rm -u `id -u`:`id -g` -p 1313:1313 -v $PWD/../..:/srv/hugo factcast/factcast-hugo hugo server -s factcast-site/documentation-docsy -w --bind 0.0.0.0 --disableFastRender --log --minify --debug --navigateToChanged --templateMetrics --buildDrafts
-#)
-#
-
-
 docker run --rm -it \
   -v $PWD/../..:/src \
   -p 1313:1313 \
