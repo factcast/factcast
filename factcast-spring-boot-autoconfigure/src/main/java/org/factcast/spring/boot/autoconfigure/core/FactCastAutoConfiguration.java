3908af523c70710f6de49b282b64278096e4f5fd
otbe <otbe@users.noreply.github.com>
2023-06-08 12:46:51 +0000

    Apply formatter

--- factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/core/FactCastAutoConfiguration.java
+++ factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/core/FactCastAutoConfiguration.java
@@ -28,7 +28,6 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
--- factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/store/rds/RdsAutoConfiguration.java
+++ factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/store/rds/RdsAutoConfiguration.java
@@ -21,7 +21,6 @@ import org.factcast.store.rds.RdsDataSourceFactoryBeanPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Generated
