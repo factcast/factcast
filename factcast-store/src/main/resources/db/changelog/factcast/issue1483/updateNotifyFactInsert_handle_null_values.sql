5fecef8700fd413a1b5188a9d8a87dd1218beb55
Benjamin Otto <benjamin.otto@style-systems.de>
2023-06-14 20:34:56 +0200

    #1483-fix-liquibase: fix broken fact insertion augmentation and fact notification handling

---
+++ factcast-store/src/main/resources/db/changelog/factcast/issue1483/updateNotifyFactInsert_handle_null_values.sql
@@ -0,0 +1,43 @@
+-- looks like pg may starve if we're replacing a function used in a trigger that is busy.
+-- so even if we'd risk to loose a notify, we're now dropping the trigger first
+
+DROP TRIGGER IF EXISTS tr_deferred_fact_insert ON fact;
+CREATE OR REPLACE FUNCTION notifyFactInsert() RETURNS trigger AS
+
+$$
+DECLARE
+    notified    BOOLEAN;
+    ns          varchar;
+    type        varchar;
+    idenditfier varchar;
+BEGIN
+
+    ns := NEW.header ->> 'ns';
+    type := NEW.header ->> 'type';
+    -- CONCAT_WS ignores null values and concatenates with "."
+    -- postgres 14+ only supports a subset of chars for custom variable names, see https://github.com/postgres/postgres/blob/f9f31aa91f82df863a35354893978e1937863d7c/src/backend/utils/misc/guc.c#L1069
+    idenditfier := REPLACE(CONCAT_WS('.', 'myvars.facttrigger', ns, type), '-', '_');
+
+    notified := NULLIF(current_setting(idenditfier, TRUE), '');
+
+    IF notified IS NULL THEN
+        perform set_config(idenditfier, 'TRUE', TRUE);
+        PERFORM pg_notify('fact_insert', json_build_object(
+                'ser', NEW.ser,
+                'header', NEW.header,
+                'txId', txid_current(),
+                'ns', ns,
+                'type', type
+            )::text);
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE CONSTRAINT TRIGGER tr_deferred_fact_insert
+    AFTER INSERT
+    ON fact DEFERRABLE INITIALLY DEFERRED
+    FOR EACH ROW
+EXECUTE PROCEDURE notifyFactInsert();
