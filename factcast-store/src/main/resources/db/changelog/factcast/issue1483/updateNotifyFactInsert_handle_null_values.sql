604367870f7496766d5d7aac0c0625e53364a310
Benjamin Otto <benjamin.otto@style-systems.de>
2023-06-15 17:18:45 +0200

    #1483-fix-liquibase: use md5 for variable name

--- factcast-store/src/main/resources/db/changelog/factcast/issue1483/updateNotifyFactInsert_handle_null_values.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue1483/updateNotifyFactInsert_handle_null_values.sql
@@ -6,22 +6,22 @@ CREATE OR REPLACE FUNCTION notifyFactInsert() RETURNS trigger AS
 
 $$
 DECLARE
-    notified    BOOLEAN;
-    ns          varchar;
-    type        varchar;
-    idenditfier varchar;
+    notified   BOOLEAN;
+    ns         varchar;
+    type       varchar;
+    identifier varchar;
 BEGIN
 
     ns := NEW.header ->> 'ns';
     type := NEW.header ->> 'type';
-    -- CONCAT_WS ignores null values and concatenates with "."
     -- postgres 14+ only supports a subset of chars for custom variable names, see https://github.com/postgres/postgres/blob/f9f31aa91f82df863a35354893978e1937863d7c/src/backend/utils/misc/guc.c#L1069
-    idenditfier := REPLACE(CONCAT_WS('.', 'myvars.facttrigger', ns, type), '-', '_');
+    -- so we're using md5 to hash the namespace and type, its reasonable fast
+    identifier := CONCAT('myvars.facttrigger.', CONCAT('fc', MD5(CONCAT(ns, type))));
 
-    notified := NULLIF(current_setting(idenditfier, TRUE), '');
+    notified := NULLIF(current_setting(identifier, TRUE), '');
 
     IF notified IS NULL THEN
-        perform set_config(idenditfier, 'TRUE', TRUE);
+        perform set_config(identifier, 'TRUE', TRUE);
         PERFORM pg_notify('fact_insert', json_build_object(
                 'ser', NEW.ser,
                 'header', NEW.header,
