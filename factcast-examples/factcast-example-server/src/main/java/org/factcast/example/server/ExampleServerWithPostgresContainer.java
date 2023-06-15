585992938efea8500a0fe693478b09905bd4ee0f
Benjamin Otto <benjamin.otto@style-systems.de>
2023-06-15 17:39:33 +0200

    #1483-fix-liquibase: cleanup

--- factcast-examples/factcast-example-server/src/main/java/org/factcast/example/server/ExampleServerWithPostgresContainer.java
+++ factcast-examples/factcast-example-server/src/main/java/org/factcast/example/server/ExampleServerWithPostgresContainer.java
@@ -43,7 +43,7 @@ public class ExampleServerWithPostgresContainer {
 
   private static void startPostgresContainer() {
     log.info("Trying to start postgres testcontainer");
-    PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:15");
+    PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:11.4");
     postgres.start();
     String url = postgres.getJdbcUrl();
     System.setProperty("spring.datasource.driver-class-name", Driver.class.getName());
