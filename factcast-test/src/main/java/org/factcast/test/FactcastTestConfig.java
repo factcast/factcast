f524fd1e69d00620a0102b2f2a6628601c37ddfd
Benjamin Otto <benjamin.otto@style-systems.de>
2023-06-15 08:40:43 +0200

    #2307-playground: add execution of multiple pg versions

--- factcast-test/src/main/java/org/factcast/test/FactcastTestConfig.java
+++ factcast-test/src/main/java/org/factcast/test/FactcastTestConfig.java
@@ -26,7 +26,7 @@ import lombok.With;
 public @interface FactcastTestConfig {
   String factcastVersion() default Config.FACTCAST_VERSION;
 
-  String postgresVersion() default Config.POSTGRES_VERSION;
+  String postgresVersion() default "";
 
   String configDir() default Config.CONFIG_DIR;
 
@@ -38,11 +38,11 @@ public @interface FactcastTestConfig {
     String configDir;
 
     static final String FACTCAST_VERSION = "latest";
-    static final String POSTGRES_VERSION = "11.5";
     static final String CONFIG_DIR = "./config";
 
     static Config defaults() {
-      return new Config(FACTCAST_VERSION, POSTGRES_VERSION, CONFIG_DIR);
+      return new Config(
+          FACTCAST_VERSION, System.getProperty("postgres.version", "11.5"), CONFIG_DIR);
     }
 
     static Config from(@NonNull FactcastTestConfig e) {
