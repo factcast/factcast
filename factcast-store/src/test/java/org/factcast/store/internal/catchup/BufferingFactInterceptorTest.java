7a6fe87648037c26671290a605499ad1912b4772
Benjamin Otto <benjamin.otto@style-systems.de>
2023-08-31 14:47:52 +0200

    #2340: tests

--- factcast-store/src/test/java/org/factcast/store/internal/catchup/BufferingFactInterceptorTest.java
+++ factcast-store/src/test/java/org/factcast/store/internal/catchup/BufferingFactInterceptorTest.java
@@ -20,7 +20,6 @@ import static org.mockito.Mockito.*;
 import io.micrometer.core.instrument.Counter;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import lombok.NonNull;
@@ -29,7 +28,6 @@ import org.factcast.core.subscription.SubscriptionImpl;
 import org.factcast.core.subscription.transformation.FactTransformerService;
 import org.factcast.core.subscription.transformation.FactTransformers;
 import org.factcast.core.subscription.transformation.TransformationRequest;
-import org.factcast.store.internal.Pair;
 import org.factcast.store.internal.PgMetrics;
 import org.factcast.store.internal.filter.FactFilter;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,11 +44,7 @@ public class BufferingFactInterceptorTest {
   @Mock private FactFilter filter;
   @Mock private SubscriptionImpl targetSubscription;
 
-  @Mock
-  private Pair<TransformationRequest, CompletableFuture<Fact>>
-      transformationRequestCompletableFuturePair;
-
-  private ExecutorService es = Executors.newSingleThreadExecutor();
+  private final ExecutorService es = Executors.newSingleThreadExecutor();
   @Mock private @NonNull PgMetrics metrics;
   private BufferingFactInterceptor uut;
 
@@ -80,12 +74,73 @@ public class BufferingFactInterceptorTest {
       when(service.transform(anyList())).thenReturn(List.of(fact));
 
       uut.accept(fact);
+      verifyNoInteractions(targetSubscription);
       uut.accept(fact);
+      verifyNoInteractions(targetSubscription);
       uut.accept(fact);
 
       // maxBufferSize is 3, so we expect a flush = 3 notifyElement call
       verify(targetSubscription, times(3)).notifyElement(fact);
       verify(service).transform(List.of(transformationRequest));
+    }
+
+    @Test
+    void afterSwitchingToBufferingModeFlushesAfterMaxSizeReached() {
+      final var factToTransform = mock(Fact.class);
+      final var noopFact = mock(Fact.class);
+      final var factToTransform2 = mock(Fact.class);
+
+      when(metrics.counter(any())).thenReturn(mock(Counter.class));
+      when(filter.test(any())).thenReturn(true);
+
+      when(transformers.prepareTransformation(any()))
+          .thenReturn(transformationRequest, null, transformationRequest);
+      when(transformationRequest.toTransform()).thenReturn(factToTransform, factToTransform2);
+      when(factToTransform.id()).thenReturn(UUID.randomUUID());
+      when(factToTransform2.id()).thenReturn(UUID.randomUUID());
+
+      when(service.transform(anyList())).thenReturn(List.of(factToTransform, factToTransform2));
+
+      uut.accept(factToTransform);
+      verifyNoInteractions(targetSubscription);
+      uut.accept(noopFact);
+      verifyNoInteractions(targetSubscription);
+      uut.accept(factToTransform2);
+
+      // maxBufferSize is 3, so we expect a flush = 3 notifyElement call
+      verify(targetSubscription).notifyElement(factToTransform);
+      verify(targetSubscription).notifyElement(noopFact);
+      verify(targetSubscription).notifyElement(factToTransform2);
+      verify(service).transform(List.of(transformationRequest, transformationRequest));
+    }
+  }
+
+  @Nested
+  class DirectMode {
+
+    @Mock private Fact fact;
+
+    @BeforeEach
+    void setUp() {
+      uut =
+          new BufferingFactInterceptor(
+              service, transformers, filter, targetSubscription, 50, metrics, es);
+    }
+
+    @Test
+    void simplePassThrough() {
+      when(metrics.counter(any())).thenReturn(mock(Counter.class));
+      when(transformers.prepareTransformation(any())).thenReturn(null);
+      when(filter.test(any())).thenReturn(true);
+
+      uut.accept(fact);
+      verify(targetSubscription).notifyElement(fact);
+      uut.accept(fact);
+      verify(targetSubscription, times(2)).notifyElement(fact);
+      uut.accept(fact);
+      verify(targetSubscription, times(3)).notifyElement(fact);
+
+      verifyNoInteractions(service);
     }
   }
 }
