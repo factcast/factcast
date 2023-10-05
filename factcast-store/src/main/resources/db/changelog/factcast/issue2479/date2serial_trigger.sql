976644e94465f838535fb65194628c9c1a3c503c
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2023-10-05 20:21:36 +0200

    #2479: setup and migrate fact data to date2serial lookup table

---
+++ factcast-store/src/main/resources/db/changelog/factcast/issue2479/date2serial_trigger.sql
@@ -0,0 +1,13 @@
+DROP TRIGGER IF EXISTS tr_fact_date2serial ON fact CASCADE; 
+DROP FUNCTION IF EXISTS maintain_fact_date CASCADE;
+
+CREATE TABLE IF NOT EXISTS tmp_fact_date_trigger (date date);
+INSERT INTO tmp_fact_date_trigger VALUES (now());
+
+CREATE FUNCTION maintain_fact_date() RETURNS trigger AS $$
+BEGIN
+    INSERT INTO date2serial VALUES (now()::date,NEW.ser) ON CONFLICT DO NOTHING; 
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER tr_fact_date2serial BEFORE INSERT ON fact FOR EACH ROW EXECUTE PROCEDURE maintain_fact_date();
