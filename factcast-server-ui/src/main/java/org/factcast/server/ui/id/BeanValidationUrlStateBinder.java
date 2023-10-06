848be00ca5d7660493741a87428865b05ea9db4c
Benjamin Otto <benjamin.otto@style-systems.de>
2023-10-06 09:49:33 +0200

    persist form into url - first attempt

---
+++ factcast-server-ui/src/main/java/org/factcast/server/ui/id/BeanValidationUrlStateBinder.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2017-2023 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.server.ui.id;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.router.QueryParameters;
+import java.net.URISyntaxException;
+import lombok.SneakyThrows;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class BeanValidationUrlStateBinder<T> extends BeanValidationBinder<T> {
+  private final ObjectMapper om = new ObjectMapper();
+  private final Class<T> beanType;
+
+  public BeanValidationUrlStateBinder(Class<T> beanType) {
+    super(beanType);
+    this.beanType = beanType;
+  }
+
+  public BeanValidationUrlStateBinder(Class<T> beanType, boolean scanNestedDefinitions) {
+    super(beanType, scanNestedDefinitions);
+    this.beanType = beanType;
+  }
+
+  @Override
+  public void writeBean(T t) throws ValidationException {
+    super.writeBean(t);
+
+    UI.getCurrent()
+        .getPage()
+        .fetchCurrentURL(
+            x -> {
+              try {
+                final var uri =
+                    UriComponentsBuilder.fromUri(x.toURI())
+                        .replaceQueryParam("state", this.writeValueAsString(t))
+                        .build();
+
+                UI.getCurrent().getPage().getHistory().replaceState(null, uri.toUriString());
+
+              } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+              }
+            });
+  }
+
+  @SneakyThrows
+  public void readFromQueryParams(QueryParameters queryParameters, T bean) {
+    final var parametersMap = queryParameters.getParameters();
+
+    if (parametersMap.containsKey("state")) {
+      this.om.readerForUpdating(bean).readValue(parametersMap.get("state").get(0));
+      readBean(bean);
+    }
+  }
+
+  @SneakyThrows
+  private String writeValueAsString(Object value) {
+    return this.om.writeValueAsString(value);
+  }
+}
