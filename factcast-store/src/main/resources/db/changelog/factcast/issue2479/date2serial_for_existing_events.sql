9d6cfd3472cd307430f410decce20e86f35c3732
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2023-10-09 16:42:27 +0200

    #2479: using a temp child-interface to factstore in order not to expose methods to grpc (yet)

--- factcast-store/src/main/resources/db/changelog/factcast/issue2479/date2serial_for_existing_events.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue2479/date2serial_for_existing_events.sql
@@ -3,8 +3,8 @@
 -- as it can take some time depending on your DB performance
 
 INSERT INTO date2serial 
-	(SELECT	fact_date(header),ser FROM fact ORDER BY ser) 
-ON CONFLICT(date) DO NOTHING;
+	(SELECT	fact_date(header) AS d, min(ser), max(ser) FROM fact GROUP BY d);
+
 
 -- for the day when the trigger was introduced, we might have a wrong serial 
 -- (not the first of the day). lets fix this:
@@ -14,17 +14,15 @@ $$
 DECLARE 
   tofix date;
   startFrom bigint;
-  firstSer bigint;
 BEGIN
  
  	tofix := (SELECT LEAST((SELECT date FROM tmp_fact_date_trigger),now()));
-  startFrom := (SELECT GREATEST((SELECT max(ser) FROM date2serial WHERE	date < tofix),0));
-  firstSer := (SELECT min(ser) FROM fact WHERE ser > startFrom AND fact_date(header) = tofix);
+  startFrom := (SELECT GREATEST((SELECT max(lastSer) FROM date2serial WHERE	date < tofix),0));
  
   INSERT INTO date2serial 
-  	VALUES (tofix,firstSer) 
+  	(SELECT fact_date(header), min(ser), max(ser) FROM fact WHERE fact_date(header) = tofix AND ser > startFrom GROUP BY fact_date(header)) 
   	ON CONFLICT(date) DO UPDATE 
-  	SET ser=excluded.ser;
+  		SET firstSer=excluded.firstSer, lastSer=excluded.lastSer;
  
   DELETE FROM tmp_fact_date_trigger;
 END; 
--- factcast-store/src/main/resources/db/changelog/factcast/issue2479/date2serial_lookup.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue2479/date2serial_lookup.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS date2serial (
         date date PRIMARY KEY,
-        ser bigint
+        firstSer bigint,
+        lastSer bigint
 );
