b3d7aed21f860e6be40dfda35c7c2073b8ff1371
Benjamin Otto <benjamin.otto@style-systems.de>
2023-10-15 10:08:57 +0200

    restructure frontend setup&build

--- factcast-server-ui/src/main/resources/META-INF/generated/vaadin.ts
+++
@@ -1,9 +0,0 @@
-import './vaadin-featureflags.js';
-
-import './index';
-
-import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
-
-import './theme-myapp3.global.generated.js';
-import { applyTheme } from './theme.js';
-applyTheme(document);
