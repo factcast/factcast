688632e82b1f6760d366052ea91b7a7ba2835271
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2023-10-25 12:12:47 +0200

    #2381: missing autoconf entry & sep. package for redis stuff

--- factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/redis/RedissonAutoConfiguration.java
+++ factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/redis/RedissonAutoConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.factcast.spring.boot.autoconfigure.core;
+package org.factcast.spring.boot.autoconfigure.redis;
 
 import org.factcast.factus.utils.FactusDependency;
 import org.redisson.api.RedissonClient;
