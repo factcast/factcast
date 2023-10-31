e5e8a87cf16ea4eeab24bf90aac692159cd03a6c
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2023-10-31 11:25:42 +0100

    #2381: change class to bean condition

--- factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/redis/RedissonAutoConfiguration.java
+++ factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/redis/RedissonAutoConfiguration.java
@@ -18,11 +18,11 @@ package org.factcast.spring.boot.autoconfigure.redis;
 import org.factcast.factus.utils.FactusDependency;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
-@ConditionalOnClass({RedissonClient.class})
+@ConditionalOnBean({RedissonClient.class})
 public class RedissonAutoConfiguration {
 
   @Bean
