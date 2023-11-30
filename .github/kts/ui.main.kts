b1471a140bf31c18f25858aa15988a169ac96dd0
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2023-11-30 20:43:37 +0100

    retry

--- .github/kts/ui.main.kts
+++ .github/kts/ui.main.kts
@@ -58,7 +58,7 @@ public val workflowMaven: Workflow = workflow(
 
         run(
             name = "Test - UI",
-            command = "cd factcast-server-ui ; ./mvnw -B -Dui failsafe:integration-test ",
+            command = "cd factcast-server-ui ; ../mvnw -B -Dui failsafe:integration-test ",
         )
     }
 }
