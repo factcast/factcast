abe00c6de021e90d7f357ee9dbe00cd94aa88798
Uwe Schaefer <uwe@uwe-schaefer.org>
2023-12-16 22:59:26 +0100

    kts source file definition

--- .github/kts/ui.main.kts
+++ .github/kts/ui.main.kts
@@ -22,7 +22,7 @@ public val workflowMaven: Workflow = workflow(
             branches = listOf("master"),
         ),
     ),
-    sourceFile = Paths.get(".github/kts/ui.main.kts"),
+    sourceFile =  __FILE__.toPath(),
 ) {
     job(
         id = "build",
