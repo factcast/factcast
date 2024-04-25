b1481ca1a12300bd507c22e2882fe45daf144e37
Benjamin Otto <benjamin.otto@style-systems.de>
2024-04-25 17:38:19 +0200

    #2854: add test

--- factcast-store/src/test/java/org/factcast/store/registry/validation/schema/store/PgSchedLockTest.java
+++ factcast-store/src/test/java/org/factcast/store/registry/validation/schema/store/PgSchedLockTest.java
@@ -15,17 +15,25 @@
  */
 package org.factcast.store.registry.validation.schema.store;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.factcast.store.internal.PgTestConfiguration;
 import org.factcast.test.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
@@ -37,6 +45,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @IntegrationTest
 public class PgSchedLockTest {
+  @Autowired LockProvider lockProvider;
 
   static final CountDownLatch latch = new CountDownLatch(1);
 
@@ -51,6 +60,31 @@ public class PgSchedLockTest {
 
     // and make sure it had one when executing the scheduled method
     assertTrue(rs.get().next(), "Lock should have been found");
+  }
+
+  @Test
+  @SneakyThrows
+  public void extendsLock() {
+    final var lock = lockProvider.lock(buildLockConfig());
+
+    assertThat(lock.isPresent()).isTrue();
+
+    // now we have to sleep for at least 30s, better 45s
+    Thread.sleep(Duration.ofSeconds(45).toMillis());
+
+    // this lock should still be held by the first one, so no new lock
+    assertThat(lockProvider.lock(buildLockConfig()).isEmpty()).isTrue();
+
+    lock.get().unlock();
+  }
+
+  private static @NonNull LockConfiguration buildLockConfig() {
+    return new LockConfiguration(
+        Instant.now(),
+        "hugo",
+        Duration.ofSeconds(30), // we cannot go lower than 30s here because this is the min for
+        // KeepAliveLockProvider
+        Duration.ofSeconds(2));
   }
 }
 
