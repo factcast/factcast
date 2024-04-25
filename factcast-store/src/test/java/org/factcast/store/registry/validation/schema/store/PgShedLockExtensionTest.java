f887cbcc31a6fc0b96bcb621a0ab69a459ebeb10
Benjamin Otto <benjamin.otto@style-systems.de>
2024-04-25 17:58:39 +0200

    #2854: dedicated test class

--- factcast-store/src/test/java/org/factcast/store/registry/validation/schema/store/PgShedLockExtensionTest.java
+++ factcast-store/src/test/java/org/factcast/store/registry/validation/schema/store/PgShedLockExtensionTest.java
@@ -16,64 +16,41 @@
 package org.factcast.store.registry.validation.schema.store;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.factcast.store.internal.PgTestConfiguration;
 import org.factcast.test.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ContextConfiguration(classes = {PgTestConfiguration.class, PgSchedLockTestConfiguration.class})
+@ContextConfiguration(classes = {PgTestConfiguration.class})
 @ExtendWith(SpringExtension.class)
 @IntegrationTest
-public class PgSchedLockTest {
+public class PgShedLockExtensionTest {
   @Autowired LockProvider lockProvider;
-
-  static final CountDownLatch latch = new CountDownLatch(1);
-
-  static AtomicReference<SqlRowSet> rs = new AtomicReference<>();
-
-  @Test
-  public void schedLockEffectiveWithScheduledMethod() throws InterruptedException {
-
-    // wait for the intercepted bean to finish it's work (and release the lock
-
-    latch.await(5, TimeUnit.SECONDS);
-
-    // and make sure it had one when executing the scheduled method
-    assertTrue(rs.get().next(), "Lock should have been found");
-  }
 
   @Test
   @SneakyThrows
-  public void extendsLock() {
+  void extendsLock() {
     final var lock = lockProvider.lock(buildLockConfig());
 
-    assertThat(lock.isPresent()).isTrue();
+    assertThat(lock).isPresent();
 
     // now we have to sleep for at least 30s, better 45s
     Thread.sleep(Duration.ofSeconds(45).toMillis());
 
     // this lock should still be held by the first one, so no new lock
-    assertThat(lockProvider.lock(buildLockConfig()).isEmpty()).isTrue();
+    assertThat(lockProvider.lock(buildLockConfig())).isEmpty();
 
     lock.get().unlock();
   }
@@ -85,27 +62,5 @@ public class PgSchedLockTest {
         Duration.ofSeconds(30), // we cannot go lower than 30s here because this is the min for
         // KeepAliveLockProvider
         Duration.ofSeconds(2));
-  }
-}
-
-class PgSchedLockTestConfiguration {
-  @Bean
-  public BeanToIntercept bean(JdbcTemplate tpl) {
-    return new BeanToIntercept(tpl);
-  }
-}
-
-@RequiredArgsConstructor
-@Slf4j
-class BeanToIntercept {
-  final JdbcTemplate tpl;
-
-  @Scheduled(initialDelay = 1000, fixedDelay = 1000)
-  @SchedulerLock(name = "hubba")
-  public void scheduled() throws InterruptedException {
-    log.info("hubba");
-
-    PgSchedLockTest.rs.set(tpl.queryForRowSet("select * from shedlock"));
-    PgSchedLockTest.latch.countDown();
   }
 }
