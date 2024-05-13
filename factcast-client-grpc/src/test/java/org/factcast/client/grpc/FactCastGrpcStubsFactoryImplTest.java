900e1ffbecfb36305edc07b4a0682788fbff6b61
bresciamattia <bresciamattia@users.noreply.github.com>
2024-05-13 07:47:40 +0000

    Apply formatter

--- factcast-client-grpc/src/test/java/org/factcast/client/grpc/FactCastGrpcStubsFactoryImplTest.java
+++ factcast-client-grpc/src/test/java/org/factcast/client/grpc/FactCastGrpcStubsFactoryImplTest.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.factcast.client.grpc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import io.grpc.Channel;
 import org.factcast.grpc.api.gen.RemoteFactStoreGrpc;
@@ -8,21 +26,18 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class FactCastGrpcStubsFactoryImplTest {
 
-  @Mock
-  Channel channel;
+  @Mock Channel channel;
 
   final FactCastGrpcStubsFactory uut = new FactCastGrpcStubsFactoryImpl();
 
   @Test
   public void createsBlockingStub() {
     try (MockedStatic<RemoteFactStoreGrpc> mock = mockStatic(RemoteFactStoreGrpc.class)) {
-      RemoteFactStoreGrpc.RemoteFactStoreBlockingStub stub = mock(RemoteFactStoreGrpc.RemoteFactStoreBlockingStub.class);
+      RemoteFactStoreGrpc.RemoteFactStoreBlockingStub stub =
+          mock(RemoteFactStoreGrpc.RemoteFactStoreBlockingStub.class);
       mock.when(() -> RemoteFactStoreGrpc.newBlockingStub(channel)).thenReturn(stub);
       assertEquals(stub, uut.createBlockingStub(channel));
     }
@@ -31,7 +46,8 @@ class FactCastGrpcStubsFactoryImplTest {
   @Test
   public void createsStub() {
     try (MockedStatic<RemoteFactStoreGrpc> mock = mockStatic(RemoteFactStoreGrpc.class)) {
-      RemoteFactStoreGrpc.RemoteFactStoreStub stub = mock(RemoteFactStoreGrpc.RemoteFactStoreStub.class);
+      RemoteFactStoreGrpc.RemoteFactStoreStub stub =
+          mock(RemoteFactStoreGrpc.RemoteFactStoreStub.class);
       mock.when(() -> RemoteFactStoreGrpc.newStub(channel)).thenReturn(stub);
       assertEquals(stub, uut.createStub(channel));
     }
