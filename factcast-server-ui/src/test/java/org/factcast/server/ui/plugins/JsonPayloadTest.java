91b86ab3e4ac2eb2ba406a2077f5556f1c62958a
uweschaefer <uweschaefer@users.noreply.github.com>
2024-06-13 19:19:04 +0000

    Apply formatter

--- factcast-server-ui/src/test/java/org/factcast/server/ui/plugins/JsonPayloadTest.java
+++ factcast-server-ui/src/test/java/org/factcast/server/ui/plugins/JsonPayloadTest.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.factcast.server.ui.plugins;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.ParseContext;
@@ -9,85 +27,67 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class JsonPayloadTest {
 
-    private static final String JSON = "JSON";
-    @Mock
-    private ParseContext parseContext;
-    @Mock
-    private ParseContext pathReturningContext;
-    @Mock
-    private DocumentContext documentContext;
-    @Mock
-    private DocumentContext pathReturningDocumentContext;
-    @InjectMocks
-    private JsonPayload underTest;
+  private static final String JSON = "JSON";
+  @Mock private ParseContext parseContext;
+  @Mock private ParseContext pathReturningContext;
+  @Mock private DocumentContext documentContext;
+  @Mock private DocumentContext pathReturningDocumentContext;
+  @InjectMocks private JsonPayload underTest;
 
-    @Nested
-    class WhenFindingPaths {
-        private final String PATH = "PATH";
+  @Nested
+  class WhenFindingPaths {
+    private final String PATH = "PATH";
 
-        @BeforeEach
-        void setup() {
-        }
-    }
+    @BeforeEach
+    void setup() {}
+  }
 
-    @Nested
-    class WhenFindingAnyPaths {
-        @BeforeEach
-        void setup() {
-        }
-    }
+  @Nested
+  class WhenFindingAnyPaths {
+    @BeforeEach
+    void setup() {}
+  }
 
-    @Nested
-    class WhenGetting {
-        private final String PATH = "PATH";
+  @Nested
+  class WhenGetting {
+    private final String PATH = "PATH";
 
-        @BeforeEach
-        void setup() {
-        }
-    }
+    @BeforeEach
+    void setup() {}
+  }
 
-    @Nested
-    class WhenReading {
-        private final String PATH = "PATH";
-        @Mock
-        private Class<T> clazz;
+  @Nested
+  class WhenReading {
+    private final String PATH = "PATH";
+    @Mock private Class<T> clazz;
 
-        @BeforeEach
-        void setup() {
-        }
-    }
+    @BeforeEach
+    void setup() {}
+  }
 
-    @Nested
-    class WhenAdding {
-        private final String PATH = "PATH";
-        @Mock
-        private Object value;
+  @Nested
+  class WhenAdding {
+    private final String PATH = "PATH";
+    @Mock private Object value;
 
-        @BeforeEach
-        void setup() {
-        }
-    }
+    @BeforeEach
+    void setup() {}
+  }
 
-    @Nested
-    class WhenRemoving {
-        private final String PATH = "PATH";
+  @Nested
+  class WhenRemoving {
+    private final String PATH = "PATH";
 
-        @BeforeEach
-        void setup() {
-        }
-    }
+    @BeforeEach
+    void setup() {}
+  }
 
-    @Nested
-    class WhenGettingPayload {
-        @BeforeEach
-        void setup() {
-        }
-    }
+  @Nested
+  class WhenGettingPayload {
+    @BeforeEach
+    void setup() {}
+  }
 }
