e38f6ec0a92b73797fea0a90a322e2f1d87a2e20
Mateo Murillo <mateo.murillo@prisma-capacity.eu>
2024-07-16 17:23:38 +0200

    #3031: Adding unit tests

--- factcast-snapshotcache-local-disk/src/test/java/org/factcast/core/snap/local/SnapshotDiskRepositoryImplTest.java
+++ factcast-snapshotcache-local-disk/src/test/java/org/factcast/core/snap/local/SnapshotDiskRepositoryImplTest.java
@@ -19,53 +19,182 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.io.Files;
 import java.io.File;
+import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.SneakyThrows;
+import org.awaitility.Awaitility;
 import org.factcast.factus.snapshot.Snapshot;
 import org.factcast.factus.snapshot.SnapshotId;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class SnapshotDiskRepositoryImplTest {
-  private static final long SLEEP_TIME = 5000;
+
+  private SnapshotDiskRepositoryImpl uut;
+
+  @BeforeEach
+  void setup() {
+    File tmpFolder = Files.createTempDir();
+    tmpFolder.deleteOnExit();
+
+    InMemoryAndDiskSnapshotProperties properties = new InMemoryAndDiskSnapshotProperties();
+    properties.setPathToSnapshots(tmpFolder.getAbsolutePath());
+
+    uut = new SnapshotDiskRepositoryImpl(properties);
+  }
 
   @Nested
-  class WhenLockingThroughWrite {
+  class WhenCrud {
+
     @Test
     @SneakyThrows
-    void blocksReadWhenLocked() {
-      AtomicBoolean finishedSleeping = new AtomicBoolean(false);
+    void getNotFound() {
+      // Get by the ID
+      Optional<Snapshot> response = uut.findById(SnapshotId.of("key", UUID.randomUUID()));
+      assertThat(response).isEmpty();
+    }
 
-      File tmpFolder = Files.createTempDir();
-
-      InMemoryAndDiskSnapshotProperties properties = new InMemoryAndDiskSnapshotProperties();
-      properties.setPathToSnapshots(tmpFolder.getAbsolutePath());
-
-      SnapshotDiskRepositoryImpl uut =
-          new SnapshotDiskRepositoryImpl(properties) {
-
-            @SneakyThrows
-            @Override
-            protected void doSave(Snapshot value, File target) {
-              Thread.sleep(SLEEP_TIME);
-              finishedSleeping.set(true);
-              super.doSave(value, target);
-            }
-          };
-
+    @Test
+    @SneakyThrows
+    void saveGetAndDelete() {
       final Snapshot snap =
           new Snapshot(
               SnapshotId.of("key", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
 
-      CompletableFuture.runAsync(() -> uut.save(snap)).get();
+      // Save and wait
+      uut.save(snap).get();
 
-      // Test that read is unblocked (how)
-      uut.findById(snap.id());
-      assertThat(finishedSleeping.get()).isTrue();
+      // Get by the ID
+      Optional<Snapshot> response = uut.findById(snap.id());
+      assertThat(response).isPresent();
+      assertThat(response.get()).isEqualTo(snap);
 
-      // Assert it got unblocked after the time and we get the snapshot
+      // Delete
+      uut.delete(snap.id()).get();
+
+      response = uut.findById(snap.id());
+      assertThat(response).isEmpty();
+    }
+
+    @Test
+    @SneakyThrows
+    void testMultipleFiles() {
+      final Snapshot snap1 =
+          new Snapshot(
+              SnapshotId.of("key1", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+      final Snapshot snap2 =
+          new Snapshot(
+              SnapshotId.of("key2", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+      final Snapshot snap3 =
+          new Snapshot(
+              SnapshotId.of("key3", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+
+      // Save and wait
+      uut.save(snap1).get();
+      uut.save(snap2).get();
+      uut.save(snap3).get();
+
+      // Get by the ID
+      Optional<Snapshot> response = uut.findById(snap1.id());
+      assertThat(response).isPresent();
+      assertThat(response.get()).isEqualTo(snap1);
+      response = uut.findById(snap2.id());
+      assertThat(response).isPresent();
+      assertThat(response.get()).isEqualTo(snap2);
+      response = uut.findById(snap3.id());
+      assertThat(response).isPresent();
+      assertThat(response.get()).isEqualTo(snap3);
+    }
+  }
+
+  @Nested
+  class WhenCleaningUp {
+    @Test
+    @SneakyThrows
+    void testCleanup() {
+
+      File tmpFolder = Files.createTempDir();
+      tmpFolder.deleteOnExit();
+
+      InMemoryAndDiskSnapshotProperties properties = new InMemoryAndDiskSnapshotProperties();
+      properties.setPathToSnapshots(tmpFolder.getAbsolutePath());
+      // The size of the 3 snapshots plus the tmp folder is more than 1000 bytes
+      properties.setMaxDiskSpace(1000);
+
+      uut = new SnapshotDiskRepositoryImpl(properties);
+
+      final Snapshot snap1 =
+          new Snapshot(
+              SnapshotId.of("key1", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+      final Snapshot snap2 =
+          new Snapshot(
+              SnapshotId.of("key2", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+      final Snapshot snap3 =
+          new Snapshot(
+              SnapshotId.of("key3", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+
+      // Save and wait
+      uut.save(snap1).get();
+      // Create small difference on the modified timestamp
+      Thread.sleep(50);
+      uut.save(snap2).get();
+      // Create small difference on the modified timestamp
+      Thread.sleep(50);
+      uut.save(snap3).get();
+
+      // After saving snap3 the cleanup should be triggered and snap1 (the oldest) should be deleted
+      Awaitility.await().until(() -> !uut.findById(snap1.id()).isPresent());
+    }
+
+    @Test
+    @SneakyThrows
+    void testCleanup_ModifiedDateChangedInBetween() {
+
+      File tmpFolder = Files.createTempDir();
+      tmpFolder.deleteOnExit();
+
+      InMemoryAndDiskSnapshotProperties properties = new InMemoryAndDiskSnapshotProperties();
+      properties.setPathToSnapshots(tmpFolder.getAbsolutePath());
+      // The size of the 3 snapshots plus the tmp folder is more than 1000 bytes
+      properties.setMaxDiskSpace(1000);
+
+      uut = new SnapshotDiskRepositoryImpl(properties);
+
+      final Snapshot snap1 =
+          new Snapshot(
+              SnapshotId.of("key1", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+      final Snapshot snap2 =
+          new Snapshot(
+              SnapshotId.of("key2", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+      final Snapshot snap3 =
+          new Snapshot(
+              SnapshotId.of("key3", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+
+      // Save and wait
+      uut.save(snap1).get();
+      // Create small difference on the modified timestamp
+      Thread.sleep(50);
+      uut.save(snap2).get();
+      // Create small difference on the modified timestamp
+      Thread.sleep(50);
+      uut.save(snap3).get();
+
+      // After saving snap3 the cleanup should be triggered and snap1 (the oldest) should be deleted
+      Awaitility.await().until(() -> !uut.findById(snap1.id()).isPresent());
+
+      // update the modified date of snap2
+      uut.findById(snap2.id());
+
+      final Snapshot snap4 =
+          new Snapshot(
+              SnapshotId.of("key4", UUID.randomUUID()), UUID.randomUUID(), "foo".getBytes(), false);
+
+      // Should Trigger cleanup again and delete snap3 because 2 is now not the oldest
+      uut.save(snap4).get();
+      Awaitility.await().until(() -> !uut.findById(snap3.id()).isPresent());
+      assertThat(uut.findById(snap2.id())).isPresent();
+      assertThat(uut.findById(snap4.id())).isPresent();
     }
   }
 }
