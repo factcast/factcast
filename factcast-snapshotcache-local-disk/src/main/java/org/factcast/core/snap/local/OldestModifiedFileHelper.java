6d7b71fa7128ca445a7a8e0b6ea92996b5fa643f
Mateo Murillo <mateo.murillo@prisma-capacity.eu>
2024-07-16 11:28:11 +0200

    #3031: Adding new Oldest File helper

---
+++ factcast-snapshotcache-local-disk/src/main/java/org/factcast/core/snap/local/OldestModifiedFileHelper.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.core.snap.local;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+public class OldestModifiedFileHelper {
+  private final File persistenceDirectory;
+  private final Deque<PathWithLastModifiedDate> lastModifiedPaths = new LinkedList<>();
+
+  @SneakyThrows
+  public Optional<PathWithLastModifiedDate> getOldestModifiedFile() {
+    if (lastModifiedPaths.isEmpty()) {
+      try (Stream<Path> walk = Files.walk(persistenceDirectory.toPath())) {
+        lastModifiedPaths.addAll(
+            walk.map(
+                    p -> {
+                      try {
+                        return PathWithLastModifiedDate.of(p, Files.getLastModifiedTime(p));
+                      } catch (IOException e) {
+                        return PathWithLastModifiedDate.of(
+                            p, FileTime.from(0L, TimeUnit.MILLISECONDS));
+                      }
+                    })
+                .sorted(Comparator.comparing(PathWithLastModifiedDate::lastAccessTime))
+                .limit(1000)
+                .collect(Collectors.toList()));
+      } catch (IOException e) {
+        log.error("Error getting the list of files in the snapshot directory", e);
+      }
+    }
+
+    return Optional.ofNullable(lastModifiedPaths.poll());
+  }
+
+  @Value(staticConstructor = "of")
+  public static class PathWithLastModifiedDate {
+    Path path;
+    FileTime lastAccessTime;
+
+    PathWithLastModifiedDate(Path path, FileTime lastAccessTime) {
+      this.path = path;
+      this.lastAccessTime = lastAccessTime;
+    }
+  }
+}
