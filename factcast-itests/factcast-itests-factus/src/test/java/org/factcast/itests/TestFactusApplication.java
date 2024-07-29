b5163f1d00235eaac33c42b0cae3f441fc1cb758
Mateo Murillo <mateo.murillo@prisma-capacity.eu>
2024-07-29 11:45:20 +0200

    #3031: Int tests

--- factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/TestFactusApplication.java
+++ factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/TestFactusApplication.java
@@ -24,6 +24,9 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.factcast.factus.serializer.JacksonSnapshotSerializer;
 import org.factcast.factus.serializer.SnapshotSerializer;
+import org.factcast.spring.boot.autoconfigure.snap.InMemoryAndDiskSnapshotCacheAutoConfiguration;
+import org.factcast.spring.boot.autoconfigure.snap.InMemorySnapshotCacheAutoConfiguration;
+import org.factcast.spring.boot.autoconfigure.snap.RedissonSnapshotCacheAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -34,7 +37,13 @@ import org.springframework.context.annotation.Primary;
 @SuppressWarnings("ALL")
 @SpringBootApplication
 @Slf4j
-@EnableAutoConfiguration(exclude = CompositeMeterRegistryAutoConfiguration.class)
+@EnableAutoConfiguration(
+    exclude = {
+      CompositeMeterRegistryAutoConfiguration.class,
+      RedissonSnapshotCacheAutoConfiguration.class,
+      InMemorySnapshotCacheAutoConfiguration.class,
+      InMemoryAndDiskSnapshotCacheAutoConfiguration.class,
+    })
 public class TestFactusApplication {
 
   public static void main(String[] args) {
