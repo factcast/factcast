baa22e54b8f8032092d2a2903f7abf1a87f65af8
Mateo Murillo <mateo.murillo@prisma-capacity.eu>
2024-07-29 10:26:16 +0200

    #3031: Small improvements and bugfixes

--- factcast-snapshotcache-local-disk/src/main/java/org/factcast/core/snap/local/InMemoryAndDiskSnapshotCache.java
+++ factcast-snapshotcache-local-disk/src/main/java/org/factcast/core/snap/local/InMemoryAndDiskSnapshotCache.java
@@ -29,13 +29,8 @@ public class InMemoryAndDiskSnapshotCache implements SnapshotCache {
   private final Cache<SnapshotId, Snapshot> cache;
   private final SnapshotDiskRepository snapshotDiskRepository;
 
-  public InMemoryAndDiskSnapshotCache(
-      InMemoryAndDiskSnapshotProperties props, SnapshotDiskRepository snapshotDiskRepository) {
-    cache =
-        Caffeine.newBuilder()
-            .softValues()
-            // .expireAfterAccess(Duration.ofDays(props.getDeleteSnapshotStaleForDays()))
-            .build();
+  public InMemoryAndDiskSnapshotCache(SnapshotDiskRepository snapshotDiskRepository) {
+    cache = Caffeine.newBuilder().softValues().build();
 
     this.snapshotDiskRepository = snapshotDiskRepository;
   }
