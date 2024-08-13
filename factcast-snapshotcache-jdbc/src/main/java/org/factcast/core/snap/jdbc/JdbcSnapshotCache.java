4649e3d076cf7b1f5a9e3057835a23e2fd7b4575
Mateo Murillo <mateo.murillo@prisma-capacity.eu>
2024-08-13 17:02:52 +0200

    #3046-jdbc-snap-cache: Adding new JDBC SnapshotCache

---
+++ factcast-snapshotcache-jdbc/src/main/java/org/factcast/core/snap/jdbc/JdbcSnapshotCache.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright © 2017-2020 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.core.snap.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.*;
+import javax.sql.DataSource;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.factcast.core.snap.Snapshot;
+import org.factcast.core.snap.SnapshotId;
+import org.factcast.factus.snapshot.SnapshotCache;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Transactional
+public class JdbcSnapshotCache implements SnapshotCache {
+
+  public final String createTableStatement;
+  public final String queryStatement;
+  public final String insertStatement;
+  public final String deleteStatement;
+
+  private final DataSource dataSource;
+
+  public JdbcSnapshotCache(JdbcSnapshotProperties properties, DataSource dataSource) {
+    this.dataSource = dataSource;
+
+    createTableStatement =
+        "CREATE TABLE "
+            + properties.getSnapshotsTableName()
+            + "("
+            + "key VARCHAR(512), "
+            + "uuid VARCHAR(36), "
+            + "last_fact_id VARCHAR(36), "
+            + "bytes BLOB, "
+            + "compressed boolean, "
+            + "PRIMARY KEY (key, uuid))";
+    queryStatement =
+        "SELECT * FROM " + properties.getSnapshotsTableName() + " WHERE key = ? AND uuid = ?";
+    insertStatement =
+        "INSERT INTO "
+            + properties.getSnapshotsTableName()
+            + " (key, uuid, last_fact_id, bytes, compressed) VALUES (?, ?, ?, ?, ?)";
+    deleteStatement =
+        "DELETE FROM " + properties.getSnapshotsTableName() + " WHERE key = ? AND uuid = ?";
+
+    boolean snapTableExists = doesTableExist(properties.getSnapshotsTableName());
+
+    if (properties.isCreateTablesIfMissing() && !snapTableExists) {
+      createSnapshotTable();
+    } else if (!snapTableExists) {
+      throw new IllegalStateException(
+          "Snapshots table does not exist: "
+              + properties.getSnapshotsTableName()
+              + ", and auto creation is disabled");
+    }
+  }
+
+  @SneakyThrows
+  public boolean doesTableExist(String tableName) {
+    try (Connection connection = dataSource.getConnection();
+        ResultSet rs =
+            connection
+                .getMetaData()
+                .getTables(null, null, tableName.toUpperCase(), new String[] {"TABLE"})) {
+
+      return Boolean.TRUE.equals(rs.next());
+    }
+  }
+
+  @SneakyThrows
+  private void createSnapshotTable() {
+    log.debug("Creating factus snapshot table");
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute(createTableStatement);
+    }
+
+    log.debug("Factus snapshot table created");
+  }
+
+  @Override
+  @SneakyThrows
+  public @NonNull Optional<Snapshot> getSnapshot(@NonNull SnapshotId id) {
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(queryStatement)) {
+      statement.setString(1, id.key());
+      statement.setString(2, id.uuid().toString());
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
+          SnapshotId snapshotId =
+              SnapshotId.of(resultSet.getString(0), UUID.fromString(resultSet.getString(1)));
+          return Optional.of(
+              new Snapshot(
+                  snapshotId,
+                  UUID.fromString(resultSet.getString(2)),
+                  resultSet.getBytes(3),
+                  resultSet.getBoolean(4)));
+        }
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  @Override
+  @SneakyThrows
+  public void setSnapshot(@NonNull Snapshot snapshot) {
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(insertStatement)) {
+      statement.setString(1, snapshot.id().key());
+      statement.setString(2, snapshot.id().uuid().toString());
+      statement.setString(3, snapshot.lastFact().toString());
+      statement.setBytes(4, snapshot.bytes());
+      statement.setBoolean(5, snapshot.compressed());
+
+      if (statement.executeUpdate() == 0) {
+        throw new IllegalStateException(
+            "Failed to insert snapshot into database. SnapshotId: " + snapshot.id());
+      }
+    }
+  }
+
+  @Override
+  @SneakyThrows
+  public void clearSnapshot(@NonNull SnapshotId id) {
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(deleteStatement)) {
+      statement.setString(1, id.key());
+      statement.setString(2, id.uuid().toString());
+      statement.executeUpdate();
+    }
+  }
+}
