eaef4b4d3cb7e63b540c9edd9c500d3fa83481ec
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2024-11-18 17:28:26 +0100

    override annotations

--- factcast-factus/src/main/java/org/factcast/factus/projection/TP.java
+++
@@ -1,7 +0,0 @@
-package org.factcast.factus.projection;
-
-import org.factcast.factus.event.EventObject;
-
-@OverrideNamespace(value = "x", type = EventObject.class)
-public class TP implements Projection {
-}
