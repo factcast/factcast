8ffa01e1dd1f05f082ac76462468516d8d6fc9bf
bedaka <benedikt.kaiser@prisma-capacity.eu>
2024-12-11 09:51:12 +0100

    issue3131: add s3 reportstore

---
+++ factcast-server-ui-s3-reportstore/src/main/java/org/factcast/server/ui/s3reportstore/S3ReportStore.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.server.ui.s3reportstore;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Date;
+import java.util.List;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.factcast.server.ui.port.ReportStore;
+import org.factcast.server.ui.report.Report;
+import org.factcast.server.ui.report.ReportDownload;
+import org.factcast.server.ui.report.ReportEntry;
+import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.CompletedUpload;
+import software.amazon.awssdk.transfer.s3.model.Upload;
+import software.amazon.awssdk.transfer.s3.model.UploadRequest;
+
+@RequiredArgsConstructor
+@Slf4j
+public class S3ReportStore implements ReportStore {
+
+  private final S3AsyncClient s3Client;
+  private final S3TransferManager s3TransferManager;
+  private final S3Presigner s3Presigner;
+
+  @Value("${factcast.ui.s3.bucket}")
+  private String bucketName;
+
+  @Override
+  public void save(@NonNull String userName, @NonNull Report report) {
+    final var reportKey = getReportKey(userName, report.name());
+    final var objectMapper = new ObjectMapper();
+    try {
+      UploadRequest uploadRequest =
+          UploadRequest.builder()
+              .putObjectRequest(
+                  PutObjectRequest.builder().bucket(bucketName).key(reportKey).build())
+              .requestBody(AsyncRequestBody.fromBytes(objectMapper.writeValueAsBytes(report)))
+              .build();
+
+      Upload fileUpload = s3TransferManager.upload(uploadRequest);
+
+      CompletedUpload uploadResult = fileUpload.completionFuture().join();
+      //      uploadResult.response().eTag();
+    } catch (IOException e) {
+      log.error("Failed to save report", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static String getReportKey(String userName, String reportName) {
+    return Paths.get(userName, reportName).toString();
+  }
+
+  //  @Override
+  //  public InputStreamResource getReportAsStream(@NonNull String userName, @NonNull String
+  // reportName) {
+  //    return new InputStreamResource(getReport(userName, reportName));
+  //  }
+
+  @Override
+  public ReportDownload getReport(@NonNull String userName, @NonNull String reportName) {
+    String key = getReportKey(userName, reportName);
+    checkObjectExists(key);
+
+    // Generate the pre-signed URL.
+    final var getObjectRequest = GetObjectRequest.builder().bucket(bucketName).key(key).build();
+
+    final var presignRequest =
+        GetObjectPresignRequest.builder()
+            .signatureDuration(Duration.ofHours(2))
+            .getObjectRequest(getObjectRequest)
+            .build();
+
+    URL url = s3Presigner.presignGetObject(presignRequest).url();
+
+    log.info("Generated pre-signed URL: " + url.toExternalForm());
+
+    return new ReportDownload(url, reportName);
+  }
+
+  @Override
+  public List<ReportEntry> listAllForUser(@NonNull String userName) {
+    // list all objects in the aws s3 bucket under the prefix userName.
+    ListObjectsV2Request listObjectsV2Request =
+        ListObjectsV2Request.builder().bucket(bucketName).prefix(userName).build();
+
+    try {
+      final var reportEntries =
+          s3Client.listObjectsV2(listObjectsV2Request).get().contents().stream()
+              .map(
+                  o -> new ReportEntry(getReportNameFromKey(o.key()), Date.from(o.lastModified())));
+
+      return reportEntries.toList();
+    } catch (Exception e) {
+      log.error("Failed to list reports", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void delete(@NonNull String userName, @NonNull String reportName) {
+    String reportKey = getReportKey(userName, reportName);
+    checkObjectExists(reportKey);
+
+    s3Client.deleteObject(DeleteObjectRequest.builder().bucket(bucketName).key(reportKey).build());
+  }
+
+  private void checkObjectExists(String key) {
+    try {
+      s3Client.headObject(HeadObjectRequest.builder().bucket(bucketName).key(key).build());
+    } catch (NoSuchKeyException e) {
+      throw new ReportDoesNotExistException(key);
+    }
+  }
+
+  private String getReportNameFromKey(@NonNull String key) {
+    return key.substring(key.lastIndexOf('/') + 1);
+  }
+}
---
+++ factcast-server-ui-s3-reportstore/src/main/java/org/factcast/server/ui/s3reportstore/S3ReportStoreConfiguration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.server.ui.s3reportstore;
+
+import org.factcast.server.ui.port.ReportStore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+
+@Configuration
+public class S3ReportStoreConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public S3AsyncClient s3Client() {
+    return S3AsyncClient.create();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public S3Presigner s3Presigner() {
+    return S3Presigner.create();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public S3TransferManager s3TransferManager(S3AsyncClient client) {
+    return S3TransferManager.builder().s3Client(client).build();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  ReportStore s3ReportStore(
+      S3AsyncClient s3Client, S3TransferManager s3TransferManager, S3Presigner s3Presigner) {
+    return new S3ReportStore(s3Client, s3TransferManager, s3Presigner);
+  }
+}
