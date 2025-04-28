bbb6c540cfb62b2d9b1b55d1b1c5416ecb7274fc
Benedikt Kaiser <benedikt.kaiser@prisma-capacity.eu>
2025-04-28 14:15:20 +0200

    #3612-evict-transformation-cache-on-change: Add integration test

--- factcast-store/src/test/java/org/factcast/store/internal/listen/PgListenerIntegrationTest.java
+++ factcast-store/src/test/java/org/factcast/store/internal/listen/PgListenerIntegrationTest.java
@@ -190,4 +190,107 @@ class PgListenerIntegrationTest {
       }
     }
   }
+
+  @Nested
+  class FactUpdateTrigger {
+
+    @Autowired PgConnectionSupplier pgConnectionSupplier;
+    @Autowired EventBus eventBus;
+    @Autowired FactStore factStore;
+
+    @AfterEach
+    @SneakyThrows
+    void unregisterListener() {
+      registerTestAsListener(pgConnectionSupplier.get("test"), "UNLISTEN " + CHANNEL_FACT_UPDATE);
+    }
+
+    @Test
+    @SneakyThrows
+    void containsTransactionId() {
+      // INIT
+      var pc = pgConnectionSupplier.get("test");
+
+      // let us also register as LISTENER
+      registerTestAsListener(pc, LISTEN_UPDATE_CHANNEL_SQL);
+
+      // also register on event bus
+      var events = new UpdateNotificationEventCollector();
+      eventBus.register(events);
+
+      UUID id1 = UUID.randomUUID();
+      UUID id2 = UUID.randomUUID();
+      UUID id3 = UUID.randomUUID();
+
+      factStore.publish(
+          List.of(
+              Fact.builder()
+                  .ns("test")
+                  .type("listenerTest1")
+                  .id(id1)
+                  .version(1)
+                  .build("{\"value\":\"1\"}"),
+              Fact.builder()
+                  .ns("test")
+                  .type("listenerTest1")
+                  .id(id2)
+                  .version(2)
+                  .build("{\"value\":\"1\"}"),
+              Fact.builder()
+                  .ns("test")
+                  .type("listenerTest2")
+                  .id(id3)
+                  .version(2)
+                  .build("{\"value\":\"1\"}")));
+
+      // RUN
+      final var statement =
+          pc.prepareStatement(
+              String.format(
+                  "UPDATE fact SET payload = '{\"value\":\"2\"}' WHERE header @> '{\"type\": \"%s\"}'",
+                  "listenerTest1"));
+      statement.executeUpdate();
+
+      // ASSERT
+      // first, check trigger
+      var notifications = pc.getNotifications(5_000);
+
+      assertThat(notifications)
+          .extracting(PGNotification::getName)
+          .hasSize(2)
+          .allMatch(CHANNEL_FACT_UPDATE::equals);
+
+      assertThat(notifications)
+          .extracting(n -> FactCastJson.readTree(n.getParameter()))
+          .anySatisfy(
+              n -> {
+                var h = n.get("header");
+                assertThat(h.get("ns").asText()).isEqualTo("test");
+                assertThat(h.get("type").asText()).isEqualTo("listenerTest1");
+                assertThat(h.get("id").asText()).isEqualTo(id1.toString().toLowerCase());
+              })
+          .anySatisfy(
+              n -> {
+                var h = n.get("header");
+                assertThat(h.get("ns").asText()).isEqualTo("test");
+                assertThat(h.get("type").asText()).isEqualTo("listenerTest1");
+                assertThat(h.get("id").asText()).isEqualTo(id2.toString().toLowerCase());
+              });
+    }
+
+    public class UpdateNotificationEventCollector {
+      @Getter final List<FactUpdateNotification> signals = new ArrayList<>();
+
+      @SuppressWarnings("unused")
+      @Subscribe
+      public void onEvent(FactUpdateNotification ev) {
+        signals.add(ev);
+      }
+    }
+
+    private void registerTestAsListener(PgConnection pc, String listenSql) throws SQLException {
+      try (var ps = pc.prepareStatement(listenSql)) {
+        ps.execute();
+      }
+    }
+  }
 }
