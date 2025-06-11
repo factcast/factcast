9a80562a8df9447133c5b3162ada16c95a2bf40a
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2025-06-11 15:14:02 +0200

    #3770: allow type=*

--- factcast-core/src/main/java/org/factcast/core/spec/FactSpecCoordinates.java
+++ factcast-core/src/main/java/org/factcast/core/spec/FactSpecCoordinates.java
@@ -75,4 +75,16 @@ public class FactSpecCoordinates {
 
     return new FactSpecCoordinates(_ns, _type, version);
   }
+
+  public boolean matches(@NonNull FactSpecCoordinates key) {
+    if (key.ns.equals(ns) || key.ns.equals("*") || ns.equals("*")) {
+      if (key.type.equals(type) || key.type.equals("*") || type.equals("*")) {
+        if (key.version == version || key.version == 0 || version == 0) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
 }
