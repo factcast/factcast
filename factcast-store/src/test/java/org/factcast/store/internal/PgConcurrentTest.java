47dd740af8a100e9533926337db593d5788b78ce
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2025-08-27 15:05:32 +0200

    #3951: build trigger

--- factcast-store/src/test/java/org/factcast/store/internal/PgConcurrentTest.java
+++ factcast-store/src/test/java/org/factcast/store/internal/PgConcurrentTest.java
@@ -52,7 +52,7 @@ public class PgConcurrentTest {
     return Fact.builder().ns("concurrenttest").id(UUID.randomUUID()).type("lonely").build("{}");
   }
 
-  @RepeatedTest(20) // suspected of being flaky on github runners
+  @RepeatedTest(50) // suspected of being flaky on github runners
   void testConcurrent() throws Exception {
     // prepare facts
     List<Fact> factsForAsyncBatchPublish =
