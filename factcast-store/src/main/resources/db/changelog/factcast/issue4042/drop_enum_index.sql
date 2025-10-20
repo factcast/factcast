e13d32996bbda5fe1046334c11aa9d7c17c592af
Uwe Schaefer <uwe@uwe-schaefer.org>
2025-10-20 18:33:36 +0200

    added concurrently

--- factcast-store/src/main/resources/db/changelog/factcast/issue4042/drop_enum_index.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4042/drop_enum_index.sql
@@ -1,2 +1,2 @@
 -- we're using published_schema_versions instead of the fact table by now, so that this index is no longer needed.
-DROP INDEX IF EXISTS index_for_enumeration;
+DROP INDEX CONCURRENTLY IF EXISTS index_for_enumeration;
