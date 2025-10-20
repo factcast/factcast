aaeb35d4a36017c35d10ccbdf95f3199c1dc5ba1
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2025-10-20 18:27:59 +0200

    Update factcast-store/src/main/resources/db/changelog/factcast/issue4042/drop_enum_index.sql
    Co-authored-by: Christian Flamm <alcc@gmx.de>

--- factcast-store/src/main/resources/db/changelog/factcast/issue4042/drop_enum_index.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4042/drop_enum_index.sql
@@ -1,2 +1,2 @@
 -- we're using published_schema_versions instead of the fact table by now, so that this index is no longer needed.
-DROP INDEX index_for_enumeration;
+DROP INDEX IF EXISTS index_for_enumeration;
