474f285ff30fbe15b3b5f0075f2c04a9321fa431
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2025-11-04 13:24:52 +0100

    #4076: remove dropping of column for backward comp.

--- factcast-store/src/main/resources/db/changelog/factcast/issue4046/migrate_transformationcache_lastaccesses.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4046/migrate_transformationcache_lastaccesses.sql
@@ -13,8 +13,12 @@ ON CONFLICT DO NOTHING;
 CREATE UNIQUE INDEX IF NOT EXISTS idx_transformationcache_access_key ON transformationcache_access (cache_key);
 CREATE INDEX IF NOT EXISTS idx_transformationcache_access_lastaccess ON transformationcache_access USING BTREE (last_access DESC);
 
--- the dropping of the deprecated column is in a separate file as we might decide to leave it there depending on the
--- number of rows existing
+-- the dropping of the deprecated column should be done manually, once rollback to a version before is no longer necessary
+--
+-- Also, be warned, that this might take quite a while and create quite some I/O
+--
+-- ALTER table transformationcache
+--     drop column last_access;
 
 
 
--- factcast-store/src/main/resources/db/changelog/factcast/issue4046/transformationcache_drop_lastaccesses.sql
+++
@@ -1,4 +0,0 @@
--- this might take a while and create quite some I/O
-
-ALTER table transformationcache
-    drop column last_access;
