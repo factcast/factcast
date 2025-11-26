5941e0a256d0f3ac30060058520626e2cef15d22
bedaka <33041245+bedaka@users.noreply.github.com>
2025-11-26 14:14:37 +0000

    Apply formatter

--- factcast-server-ui-s3-reportstore/src/test/java/org/factcast/server/ui/s3reportstore/S3MultipartOutputStreamTest.java
+++ factcast-server-ui-s3-reportstore/src/test/java/org/factcast/server/ui/s3reportstore/S3MultipartOutputStreamTest.java
@@ -170,7 +170,6 @@ class S3MultipartOutputStreamTest {
           .isInstanceOf(exception.getClass())
           .hasMessage(exception.getMessage());
 
-
       verify(s3).abortMultipartUpload(abortUploadCaptor.capture());
 
       final var abortRequest = abortUploadCaptor.getValue();
