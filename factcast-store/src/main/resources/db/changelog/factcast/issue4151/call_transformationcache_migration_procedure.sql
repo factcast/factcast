0858007183eb21eff6e0ec099e2615cb69800fc2
Mattia Brescia <9354343-mattiabrescia@users.noreply.gitlab.com>
2025-12-19 09:28:28 +0100

    #4151: improve migration procedure

--- factcast-store/src/main/resources/db/changelog/factcast/issue4151/call_transformationcache_migration_procedure.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4151/call_transformationcache_migration_procedure.sql
@@ -1,2 +1,2 @@
--- with a batch of 10k and a sleep of 10ms between batches, we expect to migrate 1mil records within a few seconds
-CALL migrate_transformationcache_batch(10000);
+-- with a batch of 100k and a sleep of 100ms between batches, we expect to migrate 1mil records within a few seconds
+CALL migrate_transformationcache_batch(100000);
--- factcast-store/src/main/resources/db/changelog/factcast/issue4151/transformationcache_migration_procedure.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4151/transformationcache_migration_procedure.sql
@@ -2,28 +2,40 @@
 CREATE UNIQUE INDEX IF NOT EXISTS idx_transformationcache_access_key ON transformationcache_access (cache_key);
 CREATE INDEX IF NOT EXISTS idx_transformationcache_access_lastaccess ON transformationcache_access USING BTREE (last_access DESC);
 
-CREATE OR REPLACE PROCEDURE migrate_transformationcache_batch(batch_size INTEGER)
-
+CREATE OR REPLACE PROCEDURE migrate_transformationcache_batch(batch_size integer DEFAULT 10000)
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    v_rows INTEGER;
+    v_last_access timestamp := '-infinity';
+    v_last_key text := '';
+    v_rows int;
 BEGIN
     LOOP
-        INSERT INTO transformationcache_access(cache_key, last_access)
-        SELECT transformationcache.cache_key, transformationcache.last_access::date FROM transformationcache
-        WHERE NOT EXISTS (
-            -- this relies on existing cache_key index on transformationcache_access
-            SELECT 1 FROM transformationcache_access WHERE transformationcache_access.cache_key = transformationcache.cache_key
-        )
-        LIMIT batch_size
-        ON CONFLICT DO NOTHING;
+        INSERT INTO transformationcache_access (cache_key, last_access)
+            SELECT t.cache_key, t.last_access::date
+            FROM transformationcache t
+            WHERE (t.last_access, t.cache_key) > (v_last_access, v_last_key)
+            ORDER BY t.last_access, t.cache_key
+            LIMIT batch_size
+        ON CONFLICT (cache_key) DO NOTHING;
 
         GET DIAGNOSTICS v_rows = ROW_COUNT;
+        RAISE NOTICE 'Inserted % rows', v_rows;
+
         EXIT WHEN v_rows = 0;
 
-        -- give db some room to breathe
-        PERFORM pg_sleep(0.01);
+        SELECT t.last_access, t.cache_key
+        INTO v_last_access, v_last_key
+        FROM transformationcache t
+        WHERE (t.last_access, t.cache_key) > (v_last_access, v_last_key)
+        ORDER BY t.last_access, t.cache_key
+        OFFSET batch_size - 1
+        LIMIT 1;
+
+        COMMIT;
+
+        -- throttling
+        PERFORM pg_sleep(0.1);
     END LOOP;
 END;
 $$;
