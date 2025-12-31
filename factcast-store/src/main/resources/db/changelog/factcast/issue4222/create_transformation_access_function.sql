29da3be6a1ebab6ff9b91310e140f6478cd0309c
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2025-12-31 12:28:36 +0100

    #4222: add updating behavior

--- factcast-store/src/main/resources/db/changelog/factcast/issue4222/create_transformation_access_function.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4222/create_transformation_access_function.sql
@@ -14,8 +14,15 @@ BEGIN
         LOOP
             INSERT INTO transformationcache_access
             VALUES (i, CURRENT_DATE)
-            ON CONFLICT DO NOTHING;
+            ON CONFLICT (cache_key) DO UPDATE
+                SET last_access = CURRENT_DATE
+            -- avoid unnecessary transactions
+            WHERE excluded.cache_key = i
+              AND excluded.last_access < CURRENT_DATE;
         END LOOP;
+
+    -- not possible in functions.
+    -- COMMIT;
 
     RETURN QUERY
         SELECT tc.header, tc.payload
