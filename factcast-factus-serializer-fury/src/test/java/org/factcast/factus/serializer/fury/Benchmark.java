794043f7f9d2d516bb1ef129bf3048db566e7f0e
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2026-01-02 16:31:24 +0100

    #4243: idea gone wild

---
+++ factcast-factus-serializer-fury/src/test/java/org/factcast/factus/serializer/fury/Benchmark.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.factus.serializer.fury;
+
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Stopwatch;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import org.factcast.factus.serializer.fury.testjson.Root;
+import org.springframework.core.io.ClassPathResource;
+
+public class Benchmark {
+  static Root root;
+  static final byte[] HUGE_JSON;
+
+  static {
+    try {
+      HUGE_JSON =
+          new ClassPathResource("/huge.json")
+              .getContentAsString(StandardCharsets.UTF_8)
+              .getBytes(StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    ObjectMapper om =
+        new ObjectMapper().enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION.mappedFeature());
+    try {
+      root = om.readValue(HUGE_JSON, Root.class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static final int ITERATIONS = 10000;
+
+  public static void main(String[] args) {
+    benchmark("uncompressed", new FurySnapshotSerializer());
+    benchmark("compressed", new LZ4FurySnapshotSerializer());
+  }
+
+  private static void benchmark(String title, FurySnapshotSerializer underTest) {
+
+    TestProjection source = new TestProjection().root(root);
+    underTest.deserialize(TestProjection.class, underTest.serialize(source));
+
+    System.out.println(title + ":");
+    System.out.println("Serialized projection size: " + underTest.serialize(source).length);
+
+    Stopwatch started = Stopwatch.createStarted();
+    for (int i = 0; i < ITERATIONS; i++) {
+      TestProjection target =
+          underTest.deserialize(TestProjection.class, underTest.serialize(source));
+      assertEquals(source.hashCode(), target.hashCode());
+    }
+    long ms = started.elapsed(TimeUnit.MILLISECONDS);
+    System.out.println("RT " + ms + " ms");
+    System.out.println("RT per execution " + (ms / (double) ITERATIONS) + " ms");
+  }
+
+  private static void assertEquals(long bar, long foo) {
+    if (foo != bar) {
+      throw new IllegalStateException();
+    }
+  }
+}
---
+++ factcast-factus-serializer-fury/src/test/java/org/factcast/factus/serializer/fury/testjson/MediaMetadata.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2017-2024 factcast.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.factcast.factus.serializer.fury.testjson;
+
+import com.fasterxml.jackson.annotation.*;
+import lombok.Data;
+import org.factcast.factus.serializer.fury.*;
+
+@Data
+@JsonAutoDetect
+public class MediaMetadata {
+
+  public Buvty5isxryd1 buvty5isxryd1;
+
+  public Hnkl36isxryd1 hnkl36isxryd1;
+
+  public N7oth5isxryd1 n7oth5isxryd1;
+
+  public Nsk69xgpcsyd1 nsk69xgpcsyd1;
+
+  @JsonProperty("9plsixgpcsyd1")
+  public _9plsixgpcsyd1 _9plsixgpcsyd1;
+
+  public Gi2fnwgpcsyd1 gi2fnwgpcsyd1;
+
+  public Hrjj2xgpcsyd1 hrjj2xgpcsyd1;
+
+  public Hg9wlxgpcsyd1 hg9wlxgpcsyd1;
+
+  public Kgsgwwgpcsyd1 kgsgwwgpcsyd1;
+
+  public Edgjrwgpcsyd1 edgjrwgpcsyd1;
+  public L5kdbxgpcsyd1 l5kdbxgpcsyd1;
+  public Zddurxgpcsyd1 zddurxgpcsyd1;
+
+  @JsonProperty("2wwnswgpcsyd1")
+  public _2wwnswgpcsyd1 _2wwnswgpcsyd1;
+
+  public Exk0w0hpcsyd1 exk0w0hpcsyd1;
+
+  public Bqb3uwgpcsyd1 bqb3uwgpcsyd1;
+
+  public Jk3lpwgpcsyd1 jk3lpwgpcsyd1;
+
+  public K7olywgpcsyd1 k7olywgpcsyd1;
+
+  public Atez5xgpcsyd1 atez5xgpcsyd1;
+
+  public Rkbh8xgpcsyd1 rkbh8xgpcsyd1;
+
+  public Abkgelwisryd1 abkgelwisryd1;
+
+  @JsonProperty("7tl9nxpisryd1")
+  public _7tl9nxpisryd1 _7tl9nxpisryd1;
+
+  @JsonProperty("2e4ty3gisryd1")
+  public _2e4ty3gisryd1 _2e4ty3gisryd1;
+
+  public Jre8pj2jsryd1 jre8pj2jsryd1;
+
+  @JsonProperty("222thktisryd1")
+  public _222thktisryd1 _222thktisryd1;
+
+  @JsonProperty("71rj760jsryd1")
+  public _71rj760jsryd1 _71rj760jsryd1;
+
+  public Ocwc28kisryd1 ocwc28kisryd1;
+}
