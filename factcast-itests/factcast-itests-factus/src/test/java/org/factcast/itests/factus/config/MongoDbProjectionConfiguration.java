ffe0f1aff55cda48e31a81a0bb322ce4df4cd713
bedaka <33041245+bedaka@users.noreply.github.com>
2026-01-27 12:05:16 +0000

    Apply formatter

--- factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/config/MongoDbProjectionConfiguration.java
+++ factcast-itests/factcast-itests-factus/src/test/java/org/factcast/itests/factus/config/MongoDbProjectionConfiguration.java
@@ -32,7 +32,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Slf4j
 @Configuration
 public class MongoDbProjectionConfiguration {
