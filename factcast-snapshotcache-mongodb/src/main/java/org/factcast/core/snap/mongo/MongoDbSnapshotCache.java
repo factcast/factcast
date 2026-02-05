a6b29cda5ea02d76059b661bce9493ce66215378
Mateo Murillo <mateo.murillo@prisma-capacity.eu>
2026-02-05 09:08:45 +0100

    Fixing async deletion

--- factcast-snapshotcache-mongodb/src/main/java/org/factcast/core/snap/mongo/MongoDbSnapshotCache.java
+++ factcast-snapshotcache-mongodb/src/main/java/org/factcast/core/snap/mongo/MongoDbSnapshotCache.java
@@ -171,7 +171,13 @@ public class MongoDbSnapshotCache implements SnapshotCache {
   public void remove(@NonNull SnapshotIdentifier id) {
     String fileName = getFileName(id);
     Bson query = Filters.eq(FILENAME_FIELD, fileName);
-    gridFSBucket.find(query).forEach(gridFSFile -> gridFSBucket.delete(gridFSFile.getId()));
+    gridFSBucket.find(query).forEach(gridFSFile -> {
+      try {
+        gridFSBucket.delete(gridFSFile.getId());
+      } catch (Exception e) {
+        log.debug("Failed to delete snapshot with filename: {}, possibly deleted by an async job before", fileName, e);
+      }
+    });
   }
 
   @VisibleForTesting
