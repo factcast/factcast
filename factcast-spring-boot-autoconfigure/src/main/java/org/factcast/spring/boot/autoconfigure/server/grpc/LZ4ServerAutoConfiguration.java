aa902cda4011e74aa7a456b5606212bc32e15b29
Benjamin Otto <benjamin.otto@style-systems.de>
2026-02-10 22:04:54 +0100

    #4262: #4326: Remove Lz4GrpcCodec import from LZ4ServerAutoConfiguration, per TODO for Spring-GRPC >1 and Spring Boot 4

--- factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/server/grpc/LZ4ServerAutoConfiguration.java
+++ factcast-spring-boot-autoconfigure/src/main/java/org/factcast/spring/boot/autoconfigure/server/grpc/LZ4ServerAutoConfiguration.java
@@ -20,12 +20,10 @@ import org.factcast.server.grpc.FactStoreGrpcService;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.*;
-import org.springframework.context.annotation.Import;
 import org.springframework.grpc.autoconfigure.server.GrpcServerAutoConfiguration;
 
 @AutoConfiguration
 @ConditionalOnClass({FactStoreGrpcService.class, Lz4GrpcCodec.class})
-@Import({Lz4GrpcCodec.class})
 @AutoConfigureBefore(GrpcServerAutoConfiguration.class)
 public class LZ4ServerAutoConfiguration {
   // TODO issue4326 re-enable with spring-grpc > 1 and SB4
