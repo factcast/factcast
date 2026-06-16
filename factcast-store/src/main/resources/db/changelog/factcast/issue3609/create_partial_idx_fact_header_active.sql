98e1e47b08d494701b399be41468fc91384bd479
Benedikt Kaiser <benedikt.kaiser@prisma-capacity.eu>
2026-06-16 09:19:54 +0200

    #3609-introduce-exclusion: issue3609: make migration and index creation dependent on fact table size

--- factcast-store/src/main/resources/db/changelog/factcast/issue3609/create_partial_idx_fact_header_active.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue3609/create_partial_idx_fact_header_active.sql
@@ -2,4 +2,5 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_fact_header_active
     ON fact USING GIN (header jsonb_path_ops)
     WHERE exclusion_reason IS NULL;
 
+-- replicate configuration from issue3755
 ALTER INDEX idx_fact_header_active SET ( fastupdate = false )
