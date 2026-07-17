d3717bf9daf9cda6a4cf103ce0ca30cfe588d44a
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2026-07-17 15:04:07 +0200

    Apply suggestion from @uweschaefer

--- factcast-store/src/main/resources/db/changelog/factcast/issue4785/notification_ser_index.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4785/notification_ser_index.sql
@@ -1,2 +1,2 @@
 -- this one is need for "all unseen"-style queries and max(ser)
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_ser ON notification (ser);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_notification_ser ON notification (ser);
