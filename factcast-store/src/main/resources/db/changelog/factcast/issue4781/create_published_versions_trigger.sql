d2fa7973797eed9765291763c6aa074203c86ffb
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2026-07-17 16:53:32 +0200

    #4781: renamed

--- factcast-store/src/main/resources/db/changelog/factcast/issue4781/create_published_versions_trigger.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue4781/create_published_versions_trigger.sql
@@ -1,15 +1,12 @@
+-- we're renaming the function as notify* is not a good name for something that
+-- does not notify at all
+--
 DROP FUNCTION IF EXISTS notifyTypeAndVersionOnInsert CASCADE;
-CREATE OR REPLACE FUNCTION notifyTypeAndVersionOnInsert() RETURNS trigger AS
+DROP FUNCTION IF EXISTS memorizeTypeAndVersionOnInsert CASCADE;
+CREATE OR REPLACE FUNCTION memorizeTypeAndVersionOnInsert() RETURNS trigger AS
 $$
 DECLARE
-    ns      varchar;
-    type    varchar;
-    version int;
 BEGIN
-    ns := NEW.header ->> 'ns';
-    type := NEW.header ->> 'type';
-    version := NEW.header ->> 'version';
-
     INSERT INTO published_schema_versions (ns, type, version)
     SELECT DISTINCT header ->> 'ns' as ns, header ->> 'type' as type, (header ->> 'version')::int as version
     FROM new_rows
@@ -19,9 +16,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER save_published_schema_version
+CREATE OR REPLACE TRIGGER tr_fact_save_published_schema_version
     AFTER INSERT
     ON fact
     REFERENCING NEW TABLE new_rows
     FOR STATEMENT
-EXECUTE FUNCTION notifyTypeAndVersionOnInsert();
+EXECUTE FUNCTION memorizeTypeAndVersionOnInsert();
