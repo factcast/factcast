5827b23455c6c2a5787574a88e0df5c2f6eee74e
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2026-07-17 11:15:57 +0200

    #4768: leanup

--- factcast-store/src/main/resources/db/changelog/factcast/issue180/dropAndRecreateInsertFactTriggerAsDeferrable.sql
+++ factcast-store/src/main/resources/db/changelog/factcast/issue180/dropAndRecreateInsertFactTriggerAsDeferrable.sql
@@ -2,8 +2,5 @@
 --changeset usr:issue180
 DROP TRIGGER IF EXISTS tr_fact_insert on fact;
 DROP TRIGGER IF EXISTS tr_deferred_fact_insert on fact;
-CREATE CONSTRAINT TRIGGER tr_deferred_fact_insert
-    AFTER INSERT
-    ON fact DEFERRABLE INITIALLY DEFERRED
-    FOR EACH ROW
-EXECUTE PROCEDURE notifyFactInsert();
+CREATE CONSTRAINT TRIGGER tr_deferred_fact_insert AFTER INSERT ON fact DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE notifyFactInsert();
+
